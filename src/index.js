5640bc1bc42aa5dd1826a3ece55fbdd99147f03a
knidarkness <knidarkness@gmail.com>
2019-09-25 15:16:48 +0300

    moved out validation declarations from index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,4 @@
-import yaml from 'js-yaml';
-import fs from 'fs';
+import { validate as validateText, validateFromFile } from './validate';
 
-import traverse from './traverse';
-import OpenAPIRoot from './validators';
-
-
-export const validate = (yamlData, filePath) => {
-  let document;
-  try {
-    document = yaml.safeLoad(yamlData);
-  } catch (ex) {
-    throw new Error("Can't load yaml file");
-  }
-  if (!document.openapi) return [];
-  const result = traverse(document, OpenAPIRoot, yamlData, filePath);
-  return result;
-};
-
-export const validateFromFile = (fName) => {
-  const resolvedFileName = fName; // path.resolve(fName);
-  const doc = fs.readFileSync(resolvedFileName, 'utf-8');
-  const validationResult = validate(doc, resolvedFileName);
-  return validationResult;
-};
+export default validateFromFile;
+export const validate = validateText;
