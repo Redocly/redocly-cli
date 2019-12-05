fe4c7253c335d41248692974791967a868142f39
knidarkness <knidarkness@gmail.com>
2019-12-05 18:59:53 +0200

    fix: error output

--- definitions/typeExtension.js
+++ definitions/typeExtension.js
@@ -1,17 +1,4 @@
 module.exports = (types) => ({
   ...types,
-  OpenAPIParameter: {
-    ...types.OpenAPIParameter,
-    resolveType: (node) => (node.allOf ? 'OpenAPIParameterWithAllOf' : 'OpenAPIParameter'),
-  },
-  OpenAPIParameterWithAllOf: {
-    name: 'OpenAPIParameterWithAllOf',
-    properties: {
-      allOf: 'OpenAPIParameterPartial',
-    },
-  },
-  OpenAPIParameterPartial: {
-    ...types.OpenAPIParameter,
-    name: 'OpenAPIParameterPartial',
-  },
+  
 });
