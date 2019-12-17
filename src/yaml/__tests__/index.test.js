5c0b73f73d6583ac87adfb97e2c415846caf3ffd
knidarkness <knidarkness@gmail.com>
2019-12-17 23:36:47 +0700

    test: update tests for new config structure & execution order settings

--- src/yaml/__tests__/index.test.js
+++ src/yaml/__tests__/index.test.js
@@ -16,11 +16,11 @@ describe("getLocationByPath", () => {
     ).toMatchInlineSnapshot(`
       Object {
         "endCol": 14,
-        "endIndex": 407,
-        "endLine": 23,
+        "endIndex": 456,
+        "endLine": 25,
         "startCol": 9,
-        "startIndex": 402,
-        "startLine": 23,
+        "startIndex": 451,
+        "startLine": 25,
       }
     `);
   });
@@ -52,11 +52,11 @@ describe("getLocationByPath", () => {
     ).toMatchInlineSnapshot(`
       Object {
         "endCol": 47,
-        "endIndex": 312,
-        "endLine": 18,
+        "endIndex": 361,
+        "endLine": 20,
         "startCol": 9,
-        "startIndex": 273,
-        "startLine": 18,
+        "startIndex": 322,
+        "startLine": 20,
       }
     `);
   });
@@ -75,11 +75,11 @@ describe("getLocationByPath", () => {
     ).toMatchInlineSnapshot(`
       Object {
         "endCol": 47,
-        "endIndex": 312,
-        "endLine": 18,
+        "endIndex": 361,
+        "endLine": 20,
         "startCol": 9,
-        "startIndex": 273,
-        "startLine": 18,
+        "startIndex": 322,
+        "startLine": 20,
       }
     `);
   });
@@ -98,11 +98,11 @@ describe("getLocationByPath", () => {
     ).toMatchInlineSnapshot(`
       Object {
         "endCol": 28,
-        "endIndex": 551,
-        "endLine": 28,
+        "endIndex": 600,
+        "endLine": 30,
         "startCol": 9,
-        "startIndex": 402,
-        "startLine": 23,
+        "startIndex": 451,
+        "startLine": 25,
       }
     `);
   });
@@ -115,11 +115,12 @@ describe("getCodeFrameForLocation", () => {
       "utf-8"
     );
     expect(getCodeFrameForLocation(276, 281, source)).toMatchInlineSnapshot(`
-      "[90m1|   user:[39m
-      [90m0|     parameters:[39m
-      [90m1|       - $re[4m[31mf: '#[90m[24m/components/parameters/example'[39m
-      [90m2|     get:[39m
-      [90m3|       operationId: userGet[39m"
+      "[90m1| [39m
+      [90m0| servers:[39m
+      [90m1|   - url: 'http://example[4m[31m.org'[90m[24m[39m
+      [90m2|[39m[31m [39m
+      [90m3| paths:[39m
+      [90m4|   user:[39m"
     `);
   });
 
@@ -129,17 +130,19 @@ describe("getCodeFrameForLocation", () => {
       "utf-8"
     );
     expect(getCodeFrameForLocation(276, 425, source)).toMatchInlineSnapshot(`
-      "[90m-1|   user:[39m
-      [90m00|     parameters:[39m
-      [90m01|       - $re[4m[31mf: '#/components/parameters/example'[90m[24m[39m
-      [90m02|[39m[31m [4m[31m    get:[31m[24m[39m
-      [90m03|[39m[31m [4m[31m      operationId: userGet[31m[24m[39m
-      [90m04|[39m[31m [4m[31m      description: Get user[31m[24m[39m
-      [90m05|[39m[31m [4m[31m      responses:[31m[24m[39m
-      [90m06|[39m[31m [4m[31m        '200':[31m[24m[39m
-      [90m07| [4m[31m          descri[90m[24mption: example description[39m
-      [90m08|           content:[39m
-      [90m09|             application/json:[39m"
+      "[90m-1| [39m
+      [90m00| servers:[39m
+      [90m01|   - url: 'http://example[4m[31m.org'[90m[24m[39m
+      [90m02|[39m[31m [4m[31m[31m[24m[39m
+      [90m03|[39m[31m [4m[31mpaths:[31m[24m[39m
+      [90m04|[39m[31m [4m[31m  user:[31m[24m[39m
+      [90m05|[39m[31m [4m[31m    parameters:[31m[24m[39m
+      [90m06|[39m[31m [4m[31m      - $ref: '#/components/parameters/example'[31m[24m[39m
+      [90m07|[39m[31m [4m[31m    get:[31m[24m[39m
+      [90m08|[39m[31m [4m[31m      operationId: userGet[31m[24m[39m
+      [90m09|[39m[31m [4m[31m      description: Get user[31m[24m[39m
+      [90m10|       responses:[39m
+      [90m11|         '200':[39m"
     `);
   });
 
@@ -151,7 +154,7 @@ describe("getCodeFrameForLocation", () => {
     expect(getCodeFrameForLocation(0, 7, source)).toMatchInlineSnapshot(`
       "[90m1| [4m[31mopenapi[90m[24m: 3.0.2[39m
       [90m2| info:[39m
-      [90m3|   title: Example OpenAPI 3 definition. Valid.[39m"
+      [90m3|   x-redocly-overlay:[39m"
     `);
   });
 
@@ -163,7 +166,7 @@ describe("getCodeFrameForLocation", () => {
     expect(getCodeFrameForLocation(0, 14, source)).toMatchInlineSnapshot(`
       "[90m1|[39m[31m [4m[31mopenapi: 3.0.2[31m[24m[39m
       [90m2| info:[39m
-      [90m3|   title: Example OpenAPI 3 definition. Valid.[39m"
+      [90m3|   x-redocly-overlay:[39m"
     `);
   });
 });
