eb877b32904b32ae4c938e8506ab40b7139264de
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-01-03 17:36:10 +0530

    test: update snapshots

--- src/__tests__/validate.test.js
+++ src/__tests__/validate.test.js
@@ -9,63 +9,6 @@ describe("Traverse files", () => {
       .toMatchInlineSnapshot(`
       Array [
         Object {
-          "codeFrame": "[90m55|       bearerFormat: JWT[39m
-      [90m56|   parameters:[39m
-      [90m57|     [4m[31mexample[90m[24m:[39m
-      [90m58|       allOf:[39m
-      [90m59|         - name: bla[39m",
-          "enableCodeframe": true,
-          "file": "definitions/syntetic/syntetic-1.yaml",
-          "fromRule": "parameter-description",
-          "location": Object {
-            "endCol": 12,
-            "endIndex": 1261,
-            "endLine": 57,
-            "startCol": 5,
-            "startIndex": 1254,
-            "startLine": 57,
-          },
-          "message": "The \\"Parameter\\" object should contain \\"description\\" field.",
-          "path": Array [
-            "components",
-            "parameters",
-            "example",
-          ],
-          "pathStack": Array [
-            Object {
-              "file": "definitions/syntetic/syntetic-1.yaml",
-              "path": Array [
-                "paths",
-                "user",
-                "parameters",
-                0,
-              ],
-              "startLine": 20,
-            },
-          ],
-          "possibleAlternate": undefined,
-          "severity": 4,
-          "target": "key",
-          "value": Object {
-            "allOf": Array [
-              Object {
-                "in": "query",
-                "name": "bla",
-                "required": false,
-                "schema": Object {
-                  "type": "string",
-                },
-              },
-              Object {
-                "description": "blo",
-              },
-              Object {
-                "description": "bla",
-              },
-            ],
-          },
-        },
-        Object {
           "codeFrame": "[90m56|   parameters:[39m
       [90m57|     example:[39m
       [90m58|       [4m[31mallOf[90m[24m:[39m
@@ -242,52 +185,6 @@ describe("Traverse files", () => {
           },
         },
         Object {
-          "codeFrame": "[90m55|       bearerFormat: JWT[39m
-      [90m56|   parameters:[39m
-      [90m57|     [4m[31mexample[90m[24m:[39m
-      [90m58|       allOf:[39m
-      [90m59|         - name: bla[39m",
-          "enableCodeframe": true,
-          "file": "definitions/syntetic/syntetic-1.yaml",
-          "fromRule": "parameter-description",
-          "location": Object {
-            "endCol": 12,
-            "endIndex": 1261,
-            "endLine": 57,
-            "startCol": 5,
-            "startIndex": 1254,
-            "startLine": 57,
-          },
-          "message": "The \\"Parameter\\" object should contain \\"description\\" field.",
-          "path": Array [
-            "components",
-            "parameters",
-            "example",
-          ],
-          "pathStack": Array [],
-          "possibleAlternate": undefined,
-          "severity": 4,
-          "target": "key",
-          "value": Object {
-            "allOf": Array [
-              Object {
-                "in": "query",
-                "name": "bla",
-                "required": false,
-                "schema": Object {
-                  "type": "string",
-                },
-              },
-              Object {
-                "description": "blo",
-              },
-              Object {
-                "description": "bla",
-              },
-            ],
-          },
-        },
-        Object {
           "codeFrame": "[90m56|   parameters:[39m
       [90m57|     example:[39m
       [90m58|       [4m[31mallOf[90m[24m:[39m
@@ -428,42 +325,6 @@ describe("Traverse files", () => {
                 "description": "bla",
               },
             ],
-          },
-        },
-        Object {
-          "codeFrame": "[90m64|         - description: blo[39m
-      [90m65|         - description: bla[39m
-      [90m66|     [4m[31mgenericExample[90m[24m:[39m
-      [90m67|       name: example[39m
-      [90m68|       in: query[39m",
-          "enableCodeframe": true,
-          "file": "definitions/syntetic/syntetic-1.yaml",
-          "fromRule": "parameter-description",
-          "location": Object {
-            "endCol": 19,
-            "endIndex": 1457,
-            "endLine": 66,
-            "startCol": 5,
-            "startIndex": 1443,
-            "startLine": 66,
-          },
-          "message": "The \\"Parameter\\" object should contain \\"description\\" field.",
-          "path": Array [
-            "components",
-            "parameters",
-            "genericExample",
-          ],
-          "pathStack": Array [],
-          "possibleAlternate": undefined,
-          "severity": 4,
-          "target": "key",
-          "value": Object {
-            "in": "query",
-            "name": "example",
-            "required": true,
-            "schema": Object {
-              "type": "string",
-            },
           },
         },
         Object {
@@ -518,63 +379,6 @@ describe("Traverse files", () => {
       .toMatchInlineSnapshot(`
       Array [
         Object {
-          "codeFrame": "[90m55|       bearerFormat: JWT[39m
-      [90m56|   parameters:[39m
-      [90m57|     [4m[31mexample[90m[24m:[39m
-      [90m58|       allOf:[39m
-      [90m59|         - name: bla[39m",
-          "enableCodeframe": true,
-          "file": "definitions/syntetic/syntetic-1.yaml",
-          "fromRule": "parameter-description",
-          "location": Object {
-            "endCol": 12,
-            "endIndex": 1261,
-            "endLine": 57,
-            "startCol": 5,
-            "startIndex": 1254,
-            "startLine": 57,
-          },
-          "message": "The \\"Parameter\\" object should contain \\"description\\" field.",
-          "path": Array [
-            "components",
-            "parameters",
-            "example",
-          ],
-          "pathStack": Array [
-            Object {
-              "file": "definitions/syntetic/syntetic-1.yaml",
-              "path": Array [
-                "paths",
-                "user",
-                "parameters",
-                0,
-              ],
-              "startLine": 20,
-            },
-          ],
-          "possibleAlternate": undefined,
-          "severity": 4,
-          "target": "key",
-          "value": Object {
-            "allOf": Array [
-              Object {
-                "in": "query",
-                "name": "bla",
-                "required": false,
-                "schema": Object {
-                  "type": "string",
-                },
-              },
-              Object {
-                "description": "blo",
-              },
-              Object {
-                "description": "bla",
-              },
-            ],
-          },
-        },
-        Object {
           "codeFrame": "[90m56|   parameters:[39m
       [90m57|     example:[39m
       [90m58|       [4m[31mallOf[90m[24m:[39m
@@ -751,52 +555,6 @@ describe("Traverse files", () => {
           },
         },
         Object {
-          "codeFrame": "[90m55|       bearerFormat: JWT[39m
-      [90m56|   parameters:[39m
-      [90m57|     [4m[31mexample[90m[24m:[39m
-      [90m58|       allOf:[39m
-      [90m59|         - name: bla[39m",
-          "enableCodeframe": true,
-          "file": "definitions/syntetic/syntetic-1.yaml",
-          "fromRule": "parameter-description",
-          "location": Object {
-            "endCol": 12,
-            "endIndex": 1261,
-            "endLine": 57,
-            "startCol": 5,
-            "startIndex": 1254,
-            "startLine": 57,
-          },
-          "message": "The \\"Parameter\\" object should contain \\"description\\" field.",
-          "path": Array [
-            "components",
-            "parameters",
-            "example",
-          ],
-          "pathStack": Array [],
-          "possibleAlternate": undefined,
-          "severity": 4,
-          "target": "key",
-          "value": Object {
-            "allOf": Array [
-              Object {
-                "in": "query",
-                "name": "bla",
-                "required": false,
-                "schema": Object {
-                  "type": "string",
-                },
-              },
-              Object {
-                "description": "blo",
-              },
-              Object {
-                "description": "bla",
-              },
-            ],
-          },
-        },
-        Object {
           "codeFrame": "[90m56|   parameters:[39m
       [90m57|     example:[39m
       [90m58|       [4m[31mallOf[90m[24m:[39m
@@ -937,42 +695,6 @@ describe("Traverse files", () => {
                 "description": "bla",
               },
             ],
-          },
-        },
-        Object {
-          "codeFrame": "[90m64|         - description: blo[39m
-      [90m65|         - description: bla[39m
-      [90m66|     [4m[31mgenericExample[90m[24m:[39m
-      [90m67|       name: example[39m
-      [90m68|       in: query[39m",
-          "enableCodeframe": true,
-          "file": "definitions/syntetic/syntetic-1.yaml",
-          "fromRule": "parameter-description",
-          "location": Object {
-            "endCol": 19,
-            "endIndex": 1457,
-            "endLine": 66,
-            "startCol": 5,
-            "startIndex": 1443,
-            "startLine": 66,
-          },
-          "message": "The \\"Parameter\\" object should contain \\"description\\" field.",
-          "path": Array [
-            "components",
-            "parameters",
-            "genericExample",
-          ],
-          "pathStack": Array [],
-          "possibleAlternate": undefined,
-          "severity": 4,
-          "target": "key",
-          "value": Object {
-            "in": "query",
-            "name": "example",
-            "required": true,
-            "schema": Object {
-              "type": "string",
-            },
           },
         },
         Object {
