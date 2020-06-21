8ab2f7ceabd5e752a36ab390750c909a9a88159f
Roman Hotsiy <gotsijroman@gmail.com>
2020-06-21 12:54:20 +0300

    chore: add lib export

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export type { OAS3Rule, OASRule } from './visitors';
+export type { NodeType } from './types/index';
+
+export { listOf, mapOf } from './types/index';
+
+export { validate } from './validate';
+export { bundle } from './bundle';
+
+export { loadConfig, LintConfig, Config } from './config/config';
