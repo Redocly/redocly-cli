f688e1b5164aaa8badb1f25644320be5702b9ce6
Roman Hotsiy <gotsijroman@gmail.com>
2020-06-22 07:33:37 +0300

    chore: export more types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 export type { Oas3Rule, OasRule } from './visitors';
 export type { NodeType } from './types/index';
+export type {
+  Config,
+  TransformersConfig,
+  TypesExtensionConfig,
+  RulesConfig,
+  Plugin,
+  CustomRulesConfig,
+} from './config/config';
 
 export { listOf, mapOf } from './types/index';
 
 export { validate } from './validate';
 export { bundle } from './bundle';
 
-export { loadConfig, LintConfig, Config } from './config/config';
+export { loadConfig, LintConfig } from './config/config';
