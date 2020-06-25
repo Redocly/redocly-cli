6a1ed64ecd696cb5485d7ef72729afef414bb2d9
knidarkness <knidarkness@gmail.com>
2020-06-25 11:31:10 +0300

    chore: minor refactoring

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,7 +1,7 @@
 import { readdirSync, statSync, existsSync } from 'fs';
 import { spawnSync } from 'child_process';
 import {join} from 'path';
-import { toMatchSpecificSnapshot } from './specific-snapshot';
+import { toMatchSpecificSnapshot, addSerializer } from './specific-snapshot';
 
 
 expect.extend({
@@ -10,7 +10,12 @@ expect.extend({
   },
 });
 
-describe('Run e2e tests tests', () => {
+addSerializer({
+  test: (val) => typeof val === 'string',
+  print: (v) => v as string,
+});
+
+describe('E2E', () => {
   const contents = readdirSync(__dirname);
 
   for (const file of contents) {
@@ -18,7 +23,7 @@ describe('Run e2e tests tests', () => {
     if (statSync(testPath).isFile()) continue;
     if (!existsSync(join(testPath, '.redocly.yaml'))) continue;
     
-    const r = spawnSync('npx', ['ts-node', '../../src/cli.ts', 'lint'], {
+    const r = spawnSync('npx', ['ts-node', '--transpile-only', '../../src/cli.ts', 'lint'], {
       cwd: testPath,
       env: {
         ...process.env,
@@ -32,7 +37,7 @@ describe('Run e2e tests tests', () => {
     
     const result = `${out}\n${err}`
 
-    it(`Test ${testPath}`, () => {
+    it(file, () => {
       // we need this cause TS types not actually allows to 'extend'
       (expect(result) as any).toMatchSpecificSnapshot(join(testPath, 'snapshot.js'));
     })
