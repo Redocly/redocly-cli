113aaa7dd28d664752574def4f1e1f530ffb37a5
Sergey Dubovyk <knidarkness@gmail.com>
2020-07-09 13:21:27 +0300

    fix: trailing slash rule wrong location & '/' path (#162)

--- src/rules/common/no-path-trailing-slash.ts
+++ src/rules/common/no-path-trailing-slash.ts
@@ -3,10 +3,11 @@ import { UserContext } from '../../walk';
 
 export const NoPathTrailingSlash: Oas3Rule | Oas2Rule = () => {
   return {
-    PathItem(_path: any, { report, key }: UserContext) {
-      if ((key as string).endsWith('/')) {
+    PathItem(_path: any, { report, key, location }: UserContext) {
+      if ((key as string).endsWith('/') && key !== '/') {
         report({
           message: `\`${key}\` should not have a trailing slash.`,
+          location: location.key(),
         });
       }
     },
