e62e2eac87e76cea9086525bbf60093cdbdb9330
knidarkness <knidarkness@gmail.com>
2020-08-12 13:07:42 +0300

    feat: add configurable --profile for aws cli

--- prepareBundle.js
+++ prepareBundle.js
@@ -1,12 +1,24 @@
+const yargs = require('yargs');
+
 const { execSync } = require('child_process');
 const package = require('./package.json');
 
 const version = package.version;
-const fileName = `openapi-cli.${version}.1111tar.gz`;
+const fileName = `openapi-cli.${version}.tar.gz`;
 const fileNameLatest = `openapi-cli.latest.tar.gz`;
 
 execSync(`tar -zcvf ${fileName} dist`);
 execSync(`tar -zcvf ${fileNameLatest} dist`);
 
-execSync(`aws s3 cp ${fileName} s3://openapi-cli-dist --profile redoc.ly`);
-execSync(`aws s3 cp ${fileNameLatest} s3://openapi-cli-dist --profile redoc.ly`);
+
+const argv = yargs
+    .option('aws_profile', {
+        alias: 'p',
+        type: 'string',
+    })
+    .argv;
+
+let profile = !!argv.aws_profile ? `--profile ${argv.aws_profile}` : '';
+
+execSync(`aws s3 cp ${fileName} s3://openapi-cli-dist ${profile}`);
+execSync(`aws s3 cp ${fileNameLatest} s3://openapi-cli-dist ${profile}`);
