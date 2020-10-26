fa5ec8727be8bc1cca6f56ad99188a93f48d4713
Andriy Leliv <andrii.leliv@gmail.com>
2020-10-26 15:47:43 +0300

    feat: populate tags and tagGroups

--- src/cli/merge.ts
+++ src/cli/merge.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 import { red, blue, yellow } from 'colorette';
 import { Config, loadConfig, validate } from '..';
 import { Oas2Definition } from '../typings/swagger';
-import { Oas3Definition } from '../typings/openapi';
+import { Oas3Definition, Oas3Tag } from '../typings/openapi';
 import { getFallbackEntryPointsOrExit, handleError, getTotals, printLintTotals } from '../cli';
 import { formatProblems } from '../format/format';
 import { readYaml, writeYaml } from '../utils';
@@ -51,10 +51,7 @@ export async function handleMerge (argv: {
     const tagsPrefix = getPrefix(info, argv['prefix-tags-with-info-prop']);
     const componentsPrefix = getPrefix(info, argv['prefix-components-with-info-prop']);
 
-    if (tags) {
-      const tagsArray = tags.map(tag => tag.name)
-      populateTagGroups(entryPoint, spec, tagsArray, tagsPrefix);
-    }
+    if (tags) { populateTags(entryPoint, spec, tags, tagsPrefix); }
 
     if (info && info.description) {
       const entryPointFileName = getEntryPointFileName(entryPoint);
@@ -107,27 +104,34 @@ function getEntryPointFileName(filePath: string) {
   return path.basename(filePath, path.extname(filePath))
 }
 
-function getPrefix (info: any, prefixArg: string | undefined) {
+function getPrefix(info: any, prefixArg: string | undefined) {
   if (!prefixArg) return '';
-  if (!info[prefixArg]) {
-    process.stderr.write(yellow(`Info property is not found. The default prefix name will be used.`));
-    return '';
-  }
+  if (!info[prefixArg]) exitWithError('prefix-tags-with-info-prop argument value is not found in info section. \n');
   return info[prefixArg];
 }
 
-function populateTagGroups(entryPoint: string, spec: any, tags: string[], tagsPrefix: string) {
+function populateTags(entryPoint: string, spec: any, tags: Oas3Tag[], tagsPrefix: string) {
   const xTagGroups = 'x-tagGroups';
+  const Tags = 'tags';
+  if (!spec.hasOwnProperty(Tags)) { spec[Tags] = []; }
   if (!spec.hasOwnProperty(xTagGroups)) { spec[xTagGroups] = []; }
   const entryPointFileName = getEntryPointFileName(entryPoint);
   if (!spec[xTagGroups].some((g: any) => g.name === entryPointFileName)) {
     spec[xTagGroups].push({ name: entryPointFileName, tags: [] });
   }
   const indexGroup = spec[xTagGroups].findIndex((item: any) => item.name === entryPointFileName);
-  const entryTagName = (tag: string) => tagsPrefix ? tagsPrefix +'_'+ tag : entryPointFileName +'_'+ tag;
+
   for (const tag of tags) {
-    if (!spec[xTagGroups][indexGroup]['tags'].find((t: any) => t.name === entryTagName(tag))) {
-      spec[xTagGroups][indexGroup]['tags'].push({ name: entryTagName(tag), 'x-dispayName': tag });
+    const entryPointTagName = tagsPrefix ? tagsPrefix +'_'+ tag.name : entryPointFileName +'_'+ tag.name;
+
+    if (!spec.tags.find((t: any) => t.name === entryPointTagName)) {
+      tag['x-displayName'] = tag.name;
+      tag.name = entryPointTagName;
+      spec.tags.push(tag);
+    }
+
+    if (!spec[xTagGroups][indexGroup][Tags].find((t: any) => t === entryPointTagName)) {
+      spec[xTagGroups][indexGroup][Tags].push(entryPointTagName);
     }
   }
 }
@@ -161,8 +165,10 @@ function collectPaths(openapi: Oas3Definition, entryPoint: string, spec: any, po
       }
 
       for (const operationKey of Object.keys(spec.paths[path])) {
-        if (spec.paths[path][operationKey]['tags']) {
-          populateTagGroups(entryPoint, spec, spec.paths[path][operationKey]['tags'], tagsPrefix);
+        let { tags } = spec.paths[path][operationKey];
+        if (tags) {
+          tags = tags.map((tag: string) => ({ name: tag }));
+          populateTags(entryPoint, spec, tags, tagsPrefix);
         }
       }
     }
