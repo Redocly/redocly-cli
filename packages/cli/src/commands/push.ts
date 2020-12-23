4d63b2628fc398f5752e0c09f1db6d2c84a27112
Andriy Leliv <andrii.leliv@gmail.com>
2020-12-23 16:27:49 +0200

    chore: get signed url; upload file to s3; create hash from files;

--- packages/cli/src/commands/push.ts
+++ packages/cli/src/commands/push.ts
@@ -1,6 +1,8 @@
 import * as fs from 'fs';
+import * as path from 'path';
 import fetch from 'node-fetch';
 import { yellow, green, blue } from 'colorette';
+import { createHash } from 'crypto';
 import { RedoclyClient } from '@redocly/openapi-core';
 import { promptUser, exitWithError } from '../utils';
 
@@ -16,25 +18,74 @@ export async function handlePush (argv: {
   );
   const client = new RedoclyClient();
   await client.login(clientToken);
-  const { entrypoint, destination } = argv;
+  const { entrypoint, destination, branchName } = argv;
 
   if (!validateDestination(destination!)) {
     exitWithError(`Destination argument value is not valid, please use the right format: ${yellow('<@organization-id/api-name@api-version>')}`);
   }
 
   const [ organizationId, apiName, apiVersion ] = getDestinationProps(destination!);
-  const signedUrl = await getSignedUrl();
-  await uploadFileToS3(signedUrl, entrypoint!);
-
-  const { version } = await getDefinitionId(client, organizationId, apiName, apiVersion);
+  const filesPaths = collectFilePaths(entrypoint!);
+  const filesHash = createHashFromFiles(filesPaths);
+  const fileName = getFilename(entrypoint!);
+  const { signFileUploadCLI } = await getSignedUrl(organizationId, filesHash, fileName);
+  const { signedFileUrl, uploadedFilePath } = signFileUploadCLI;
+  await uploadFileToS3(signedFileUrl, entrypoint!);
+  const { version } = await getDefinitionId(organizationId, apiName, apiVersion);
   const { definitionId, id } = version;
   const updatePatch = {
     "sourceType": "FILE",
-    "source": JSON.stringify({ files:['spec.yaml'], root: 'spec.yaml', branchName: "test"})
+    "source": JSON.stringify({
+      files: [uploadedFilePath],
+      root: uploadedFilePath,
+      branchName
+    })
+  }
+  await client.updateDefinitionVersion(definitionId, id, updatePatch);
+
+  function getSignedUrl(organizationId: string, filesHash: string, fileName: string) {
+    return client.query(`
+      query ($organizationId: String!, $filesHash: String!, $fileName: String!) {
+        signFileUploadCLI(organizationId: $organizationId, filesHash: $filesHash, fileName: $fileName) {
+          signedFileUrl
+          uploadedFilePath
+        }
+      }
+    `, {
+      organizationId,
+      filesHash,
+      fileName
+    })
   }
 
-  // @ts-ignore
-  await client.updateDefinitionVersion(definitionId, id, updatePatch);
+  function getDefinitionId(organizationId: string, definitionName: string, versionName: string) {
+    return client.query(`
+    query ($organizationId: String!, $definitionName: String!, $versionName: String!) {
+      version: definitionVersionByOrganizationDefinitionAndName(organizationId: $organizationId, definitionName: $definitionName, versionName: $versionName) {
+        id
+        definitionId
+      }
+    }
+  `, {
+      organizationId,
+      definitionName,
+      versionName
+    })
+  }
+}
+
+function collectFilePaths(entrypoint: string) {
+  return [entrypoint];
+}
+
+function getFilename(filePath: string) {
+  return path.basename(filePath);
+}
+
+function createHashFromFiles(filePaths: string[]) {
+  let sum = createHash('sha256');
+  filePaths.forEach(file => sum.update(fs.readFileSync(file)));
+  return sum.digest('hex');
 }
 
 function validateDestination(destination: string) {
@@ -46,38 +97,16 @@ function getDestinationProps(destination: string) {
   return destination.substring(1).split(/[@\/]/);
 }
 
-function getDefinitionId(client: any, organizationId: string, definitionName: string, versionName: string) {
-  return client.query(`
-    query ($orgId: String!, $definitionName: String!, $versionName: String!) {
-      version: definitionVersionByOrganizationDefinitionAndName(organizationId: $orgId, definitionName: $definitionName, versionName: $versionName) {
-        id
-        definitionId
-      }
-    }
-  `, {
-    organizationId,
-    definitionName,
-    versionName
-  })
-}
-
-function getSignedUrl() {
-    // return query(`
-    //   Query {
-    //     signFileUploadCLI(fileName: "pets.yaml")
-    //   }
-    // `)
-  return 'https://dev-redocly-specs.s3.amazonaws.com/spec.yaml?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA27RK5XRJNVVP7OVO%2F20201104%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20201104T140939Z&X-Amz-Expires=60000&X-Amz-Signature=1264aa8fca6bf295f9a4c4dc709287e71811bd0df2c126484e4dfd95ab3236b5&X-Amz-SignedHeaders=host';
-}
-
 function uploadFileToS3(url: string, entrypoint: string) {
   const stats = fs.statSync(entrypoint!);
   const fileSizeInBytes = stats.size;
   let readStream = fs.createReadStream(entrypoint!);
   return fetch(url, {
     method: 'PUT',
-    //@ts-ignore
-    headers: { "Content-length": fileSizeInBytes },
+    // @ts-ignore
+    headers: {
+      'Content-length': fileSizeInBytes
+    },
     body: readStream
   })
 }
