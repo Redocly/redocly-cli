a05906b8d2c410b8c9b3828b2a0583860a9c9872
Anton_Kozachuk <anton.kozachuk@cloudmobilizd.com>
2021-05-18 19:19:43 +0300

    fix: paths-kebab-case message and snake_case

--- packages/core/src/rules/common/__tests__/paths-kebab-case.test.ts
+++ packages/core/src/rules/common/__tests__/paths-kebab-case.test.ts
@@ -5,7 +5,7 @@ import { parseYamlToDocument, replaceSourceWithRef } from '../../../../__tests__
 import { BaseResolver } from '../../../resolve';
 
 describe('Oas3 paths-kebab-case', () => {
-  it('should report on no kebab-case path', async () => {
+  it('should report on camelCase path', async () => {
     const document = parseYamlToDocument(
       outdent`
           openapi: 3.0.0
@@ -36,7 +36,47 @@ describe('Oas3 paths-kebab-case', () => {
               "source": "foobar.yaml",
             },
           ],
-          "message": "\`/someTest\` is use kebab-case.",
+          "message": "\`/someTest\` should be in kebab-case.",
+          "ruleId": "paths-kebab-case",
+          "severity": "error",
+          "suggest": Array [],
+        },
+      ]
+    `);
+  });
+
+  it('should report on snake_case path', async () => {
+    const document = parseYamlToDocument(
+      outdent`
+          openapi: 3.0.0
+          paths:
+            /some_test:
+              get:
+                summary: List all pets
+            /test-123:
+              get:
+                summary: Test
+        `,
+      'foobar.yaml',
+    );
+
+    const results = await lintDocument({
+      externalRefResolver: new BaseResolver(),
+      document,
+      config: new LintConfig({ extends: [], rules: { 'paths-kebab-case': 'error' } }),
+    });
+
+    expect(replaceSourceWithRef(results)).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "location": Array [
+            Object {
+              "pointer": "#/paths/~1some_test",
+              "reportOnKey": true,
+              "source": "foobar.yaml",
+            },
+          ],
+          "message": "\`/some_test\` should be in kebab-case.",
           "ruleId": "paths-kebab-case",
           "severity": "error",
           "suggest": Array [],
