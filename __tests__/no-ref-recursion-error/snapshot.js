b935e877e25c756fe0ad479ddfedcc04a5b1b20a
Andriy Leliv <andrii.leliv@gmail.com>
2021-06-29 14:40:24 +0300

    chore: e2e tests: Maximum call stack size exceeded issue

---
+++ __tests__/no-ref-recursion-error/snapshot.js
@@ -0,0 +1,11 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`E2E no-ref-recursion-error 1`] = `
+
+validating /openapi.json...
+/openapi.json: validated in <test>ms
+
+Woohoo! Your OpenAPI definition is valid. 🎉
+
+
+`;
