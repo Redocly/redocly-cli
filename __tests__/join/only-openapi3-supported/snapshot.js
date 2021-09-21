8e6947989a76558739ee48938e87e5a03167c5fd
Andriy Leliv <andrii.leliv@gmail.com>
2021-09-21 11:00:38 +0300

    test: join - only openapi3 supported

---
+++ __tests__/join/only-openapi3-supported/snapshot.js
@@ -0,0 +1,10 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`E2E join only-openapi3-supported 1`] = `
+
+Only OpenAPI 3 is supported: /Users/andriyl/Projects/Redocly/openapi-cli/__tests__/join/only-openapi3-supported/foo.yaml 
+
+
+
+
+`;
