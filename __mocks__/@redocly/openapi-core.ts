f4bfbb083c17bb60af25681b15bf890973a485bb
Andriy Leliv <andrii.leliv@gmail.com>
2021-09-23 09:38:41 +0300

    first unit test for join command + mocks

--- __mocks__/@redocly/openapi-core.ts
+++ __mocks__/@redocly/openapi-core.ts
@@ -15,8 +15,27 @@ export const RedoclyClient = jest.fn(() => __redoclyClient);
 export const loadConfig = jest.fn(() => ({
   configFile: null,
   lint: { skipRules: jest.fn(), skipPreprocessors: jest.fn(), skipDecorators: jest.fn() },
+  apiDefinitions: []
 }));
 export const lint = jest.fn();
 export const bundle = jest.fn(() => ({ bundle: { parsed: null }, problems: null }));
 export const getTotals = jest.fn(() => ({ errors: 0 }));
 export const formatProblems = jest.fn();
+export const BaseResolver = jest.fn(() => ({
+  resolveDocument: jest.fn(() => {
+    return {
+      source: {
+        absoluteRef: null
+      },
+      config: {
+        "http": {
+          "headers":[]
+        },
+        "apiDefinitions": [],
+        "cache": {}
+      }
+    }
+  })
+}));
+
+export const bundleDocument = jest.fn(() => Promise.resolve(bundle));
---
+++ packages/cli/src/__tests__/commands/join.test.ts
@@ -0,0 +1,14 @@
+import { handleJoin } from '../../commands/join';
+
+describe('join', () => {
+	it('should throw error when only one entrypoint is provided', async () => {
+    const mockExit = jest.spyOn(process, 'exit').mockImplementation();
+    const mockStdErr = jest.spyOn(process.stderr, 'write').mockImplementation();
+
+    const entrypoints = ['foo.yaml'];
+    await handleJoin({ entrypoints }, '1.0.0');
+
+    expect(mockStdErr).toHaveBeenCalledWith(`At least 2 entrypoints should be provided. \n\n`);
+    expect(mockExit).toHaveBeenCalledWith(1);
+  });
+});
