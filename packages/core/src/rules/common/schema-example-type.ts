0060544e2971abe8fbd937b7e8bd2b2c42649833
Andriy Leliv <andrii.leliv@gmail.com>
2021-10-13 12:39:02 +0300

    chore: rewrite schema-example-type rule using ajv

--- packages/core/src/rules/common/schema-example-type.ts
+++ packages/core/src/rules/common/schema-example-type.ts
@@ -1,22 +1,41 @@
-import { oasTypeOf, matchesJsonSchemaType } from '../utils';
+import { oasTypeOf } from '../utils';
 import { UserContext } from '../../walk';
+import { validateJsonSchema } from '../ajv';
+import { Oas3Schema } from '../../typings/openapi';
+import { Location } from '../../ref-utils';
 
 export const SchemaExampleType: any = () => {
   return {
-    SchemaProperties(properties: any, { report, location }: UserContext) {
-      for (let keyProp in properties) {
-        const property = properties[keyProp];
-        if (property.example && property.type) {
-          const propValueType = oasTypeOf(property.example);
-          const isTypeArray = Array.isArray(property.type);
-          const checkResult = isTypeArray
-            ? property.type.some((type: string) => matchesJsonSchemaType(property.example, type, false))
-            : matchesJsonSchemaType(property.example, property.type, false);
-
-          if (!checkResult) {
+    Schema: {
+      leave(schema: Oas3Schema, { report, location, resolve }: UserContext) {
+        if (schema.example) {
+          try {
+            const propValueType = oasTypeOf(schema.example);
+            const { valid, errors } = validateJsonSchema(
+              schema.example,
+              schema,
+              location.child('schema'),
+              location.child('example').pointer,
+              resolve,
+              false
+            );
+            if (!valid) {
+              for (let error of errors) {
+                report({
+                  message: `Expected type \`${schema.type}\` but got \`${propValueType}\`.`,
+                  location: {
+                    ...new Location(location.child('example').source, error.instancePath),
+                  },
+                  from: location,
+                  suggest: error.suggest,
+                });
+              }
+            }
+          } catch(e) {
             report({
-              message: `Expected type \`${property.type}\` but got \`${propValueType}\`.`,
-              location: location.child([keyProp, 'example']),
+              message: `Schema example validation errored: ${e.message}.`,
+              location: location.child('schema'),
+              from: location
             });
           }
         }
