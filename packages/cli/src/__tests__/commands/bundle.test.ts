d4004c2c82d46a62d3d1783c860e6706c109dbf1
Andrii Tarusin <andrii.tarusin@redocly.com>
2021-10-22 15:20:48 +0300

    test: added tests for code 0 of the bundle command

--- packages/cli/src/__tests__/commands/bundle.test.ts
+++ packages/cli/src/__tests__/commands/bundle.test.ts
@@ -1,13 +1,16 @@
 import { lint, bundle } from '@redocly/openapi-core';
 
 import { handleBundle } from '../../commands/bundle';
+import SpyInstance = jest.SpyInstance;
 
 jest.mock('@redocly/openapi-core');
 jest.mock('../../utils');
 
 describe('bundle', () => {
+  let processExitMock: SpyInstance;
+
   beforeAll(() => {
-    jest.spyOn(process, 'exit').mockImplementation();
+    processExitMock = jest.spyOn(process, 'exit').mockImplementation();
     jest.spyOn(process.stderr, 'write').mockImplementation(() => true);
   });
 
@@ -32,6 +35,21 @@ describe('bundle', () => {
     expect(bundle).toBeCalledTimes(entrypoints.length);
   });
 
+  it('exits with code 0 when bundles definitions', async () => {
+    const entrypoints = ['foo.yaml', 'bar.yaml', 'foobar.yaml'];
+
+    await handleBundle(
+      {
+        entrypoints,
+        ext: 'yaml',
+        format: 'codeframe',
+      },
+      '1.0.0',
+    );
+
+    expect(processExitMock).toHaveBeenCalledWith(0);
+  });
+
   it('bundles definitions w/ linting', async () => {
     const entrypoints = ['foo.yaml', 'bar.yaml', 'foobar.yaml'];
 
@@ -48,4 +66,37 @@ describe('bundle', () => {
     expect(lint).toBeCalledTimes(entrypoints.length);
     expect(bundle).toBeCalledTimes(entrypoints.length);
   });
+
+  it('exits with code 0 when bundles definitions w/linting w/o errors', async () => {
+    const entrypoints = ['foo.yaml', 'bar.yaml', 'foobar.yaml'];
+
+    await handleBundle(
+      {
+        entrypoints,
+        ext: 'yaml',
+        format: 'codeframe',
+        lint: true,
+      },
+      '1.0.0',
+    );
+
+    expect(processExitMock).toHaveBeenCalledWith(0);
+  });
+
+  it.skip('exits with code 1 when bundles definitions w/linting w/errors', async () => {
+    const entrypoints = ['foo.yaml', 'bar.yaml', 'foobar.yaml'];
+
+    await handleBundle(
+      {
+        entrypoints,
+        ext: 'yaml',
+        format: 'codeframe',
+        lint: true,
+      },
+      '1.0.0',
+    );
+
+    expect(processExitMock).toHaveBeenCalledWith(1);
+  });
+
 });
