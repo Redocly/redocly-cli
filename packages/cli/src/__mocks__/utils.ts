8f5551dbedaa12c311dd50c020603d1d891ae6ea
Andriy Leliv <andrii.leliv@gmail.com>
2021-10-23 11:27:31 +0300

    chore: added more test for 'get info section prefix'

--- packages/cli/src/__mocks__/utils.ts
+++ packages/cli/src/__mocks__/utils.ts
@@ -8,7 +8,6 @@ export const printExecutionTime = jest.fn();
 export const printUnusedWarnings = jest.fn();
 export const printLintTotals = jest.fn();
 export const getOutputFileName = jest.fn(() => ({ outputFile: 'test.yaml', ext: 'yaml' }));
-
 export const exitWithError = jest.fn().mockImplementation((errorMessage) => {
   throw new Error(`process.exit: ${errorMessage}`);
 });
--- packages/cli/src/__tests__/commands/join.test.ts
+++ packages/cli/src/__tests__/commands/join.test.ts
@@ -1,3 +1,4 @@
+import { yellow } from 'colorette';
 import { handleJoin, getInfoPrefix } from '../../commands/join';
 import { exitWithError } from '../../utils';
 jest.mock('../../utils');
@@ -19,10 +20,45 @@ describe('join', () => {
       expect(result).toEqual('');
     });
 
-    it('should exit with error if info section is not found', () => {
+    it('should exit with an error if info section is not found', () => {
       const errorMessage = 'Info section is not found in specification';
       expect(() => getInfoPrefix(null, 'prefix', '')).toThrow(`process.exit: ${errorMessage}`);
       expect(exitWithError).toHaveBeenCalledWith(expect.stringContaining(errorMessage));
+    });
+
+    it('should exit with an error if argument value is not found in info section.', () => {
+      const errorMessage = `${yellow(
+        `prefix-components-with-info-prop`,
+      )} argument value is not found in info section. \n`;
+      expect(() => getInfoPrefix({}, 'prefix', 'components')).toThrow(
+        `process.exit: ${errorMessage}`,
+      );
+      expect(exitWithError).toHaveBeenCalledWith(expect.stringContaining(errorMessage));
+    });
+
+    it('should exit with an error if argument value is not a string.', () => {
+      const errorMessage = `${yellow(
+        `prefix-components-with-info-prop`,
+      )} argument value should be string. \n\n`;
+      expect(() => getInfoPrefix({ test: 1 }, 'test', 'components')).toThrow(
+        `process.exit: ${errorMessage}`,
+      );
+      expect(exitWithError).toHaveBeenCalledWith(expect.stringContaining(errorMessage));
+    });
+
+    it('should exit with an error if argument value exceeds 50 characters', () => {
+      const errorMessage = `${yellow(
+        `prefix-components-with-info-prop`,
+      )} argument value length should not exceed 50 characters. \n\n`;
+      expect(() => getInfoPrefix({ test: 's'.repeat(51) }, 'test', 'components')).toThrow(
+        `process.exit: ${errorMessage}`,
+      );
+      expect(exitWithError).toHaveBeenCalledWith(expect.stringContaining(errorMessage));
+    });
+
+    it('should return info section prefix', () => {
+      const result = getInfoPrefix({ title: 'Petstore' }, 'title', 'components');
+      expect(result).toEqual('Petstore');
     });
   });
 });
