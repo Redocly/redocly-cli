3bb04e89a43907cc557206b5f6ac41ca5be6498c
Andrii Tarusin <andrii.tarusin@redocly.com>
2021-10-25 12:35:46 +0300

    fix: Made the 4xx rule to check for a 4xx code despite a default case precense. Added a test.

--- packages/core/src/rules/common/operation-4xx-response.ts
+++ packages/core/src/rules/common/operation-4xx-response.ts
@@ -5,7 +5,8 @@ export const Operation4xxResponse: Oas3Rule | Oas2Rule = () => {
   return {
     ResponsesMap(responses: Record<string, object>, { report }: UserContext) {
       const codes = Object.keys(responses);
-      if (!codes.some((code) => code === 'default' || /4[Xx0-9]{2}/.test(code))) {
+
+      if (!codes.some((code) => /4[Xx0-9]{2}/.test(code))) {
         report({
           message: 'Operation must have at least one `4xx` response.',
           location: { reportOnKey: true },
