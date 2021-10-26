8945af26171bc5549404fc2cdb8bd1be8343653c
Andriy Leliv <andrii.leliv@gmail.com>
2021-10-26 18:05:23 +0300

    chore: add ctx type

--- packages/core/src/rules/common/info-description-override.ts
+++ packages/core/src/rules/common/info-description-override.ts
@@ -1,10 +1,11 @@
 import { Oas3Decorator, Oas2Decorator } from '../../visitors';
 import { readFileAsStringSync } from '../../utils';
+import { UserContext } from '../../walk';
 
 export const InfoDescriptionOverride: Oas3Decorator | Oas2Decorator = ({ filePath }) => {
   return {
     Info: {
-      leave(info, { report, location }) {
+      leave(info, { report, location }: UserContext) {
         if (!filePath) throw new Error(`Parameter "filePath" is not provided`);
         try {
           info.description = readFileAsStringSync(filePath);
