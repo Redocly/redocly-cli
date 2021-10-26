a882ef8fcb671556d8a5b995939411a9e4287685
Andriy Leliv <andrii.leliv@gmail.com>
2021-10-26 19:33:40 +0300

    chore: error messages changes

--- packages/core/src/rules/common/tag-description-override.ts
+++ packages/core/src/rules/common/tag-description-override.ts
@@ -6,13 +6,16 @@ export const TagDescriptionOverride: Oas3Decorator | Oas2Decorator = ({ tagNames
   return {
     Tag: {
       leave(tag, { report, location }: UserContext) {
-        if (!tagNames) throw new Error(`Parameter "tagNames" is not provided`);
+        if (!tagNames)
+          throw new Error(
+            `Parameter "tagNames" is not provided for "tag-description-override" rule`,
+          );
         if (tagNames[tag.name]) {
           try {
             tag.description = readFileAsStringSync(tagNames[tag.name]);
           } catch (e) {
             report({
-              message: `Failed to read file. ${e.message}`,
+              message: `Failed to read markdown override file for tag "${tag.name}".\n${e.message}`,
               location: location.child('info').key(),
             });
           }
