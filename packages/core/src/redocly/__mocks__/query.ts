3321a4b1a5045a1dd709e48dcd0f6cadaca98654
Andrii Tarusin <andrii.tarusin@redocly.com>
2021-11-05 15:08:17 +0200

    test: added unit tests for the redocly client

---
+++ packages/core/src/redocly/__mocks__/query.ts
@@ -0,0 +1,1 @@
+export const query = jest.fn(() => Promise.resolve({foo: 'bar'}));
---
+++ packages/core/src/redocly/__tests__/redocly-client.test.ts
@@ -0,0 +1,36 @@
+import { RedoclyClient } from '@redocly/openapi-core/src/redocly/index';
+import { query } from '../query';
+
+jest.mock('../query');
+
+describe('RedoclyClient', () => {
+  const redoclyDomain = 'some-redoly-domain';
+  let redoclyClient: RedoclyClient;
+
+  beforeEach(() => {
+    (query as jest.Mock).mockClear();
+    redoclyClient = new RedoclyClient(redoclyDomain);
+  });
+
+  it('login using a passed Redocly domain', async () => {
+    await redoclyClient.login('accessToken');
+
+    expect(query).toHaveBeenCalledWith(
+      expect.anything(),
+      redoclyDomain,
+      expect.anything(),
+      expect.anything(),
+    );
+  });
+
+  it.only('receives auth header using a passed Redocly domain', async () => {
+    await redoclyClient.getAuthorizationHeader();
+
+    expect(query).toHaveBeenCalledWith(
+      expect.anything(),
+      redoclyDomain,
+      expect.anything(),
+      expect.anything(),
+    );
+  });
+});
