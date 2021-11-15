f994c0785c3570368793f98e0944c06f8744612d
Chris <78322146+chrmay@users.noreply.github.com>
2021-11-15 14:20:46 +0100

    fix: should not throw error when server url is root (#448)

--- packages/core/src/rules/oas3/no-server-trailing-slash.ts
+++ packages/core/src/rules/oas3/no-server-trailing-slash.ts
@@ -4,7 +4,7 @@ export const NoServerTrailingSlash: Oas3Rule = () => {
   return {
     Server(server, { report, location }) {
       if (!server.url) return;
-      if (server.url.endsWith('/')) {
+      if (server.url.endsWith('/') && server.url !== '/') {
         report({
           message: 'Server `url` should not have a trailing slash.',
           location: location.child(['url']),
