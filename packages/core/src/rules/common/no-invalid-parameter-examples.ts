d77f9b8927ef6bf4dc3d43fe9dca64298a68b27e
Andriy Leliv <andrii.leliv@gmail.com>
2021-11-29 19:25:49 +0200

    chore: added disallowAdditionalProperties

--- packages/core/src/rules/common/no-invalid-parameter-examples.ts
+++ packages/core/src/rules/common/no-invalid-parameter-examples.ts
@@ -2,7 +2,8 @@ import { UserContext } from '../../walk';
 import { Oas3Parameter } from '../../typings/openapi';
 import { validateExample } from '../utils';
 
-export const NoInvalidParameterExamples: any = () => {
+export const NoInvalidParameterExamples: any = (opts: any) => {
+  const disallowAdditionalProperties = opts.disallowAdditionalProperties ?? true;
   return {
     Parameter: {
       leave(parameter: Oas3Parameter, ctx: UserContext) {
@@ -12,7 +13,7 @@ export const NoInvalidParameterExamples: any = () => {
             parameter.schema!,
             ctx.location.child('example'),
             ctx,
-            false,
+            disallowAdditionalProperties,
           );
         }
 
