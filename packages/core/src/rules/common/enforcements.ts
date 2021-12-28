0bbbc1019166470df1c87edd1b8e1ec5c5da7258
Oleksii <oleksii.horbachevskyi@redocly.com>
2021-12-28 19:37:48 +0200

    chore: small refactoring

--- packages/core/src/rules/common/enforcements.ts
+++ packages/core/src/rules/common/enforcements.ts
@@ -1,75 +1,69 @@
-import * as rules from '../generic-rules';
+import * as genericRules from '../generic-rules';
+
+type Rule = {
+  name: string,
+  conditions: any,
+  description: string
+}
+
+const formRule = (rule: Rule, parts: Array<string>, enforcement: {[key: string]: any}) => {
+  const hasMutuallyRule = !!(enforcement.mutuallyExclusive || enforcement.mutuallyRequired);
+  const lastProp: string = hasMutuallyRule ? '' : parts.pop() as string; // property on which we will do the lint
+  const lastNode: string = parts.pop() as string; // node that have this property
+  return {
+    [lastNode]: function(node: any, { report, location }: any) {
+      const value = hasMutuallyRule ? node : node[lastProp];
+      // @ts-ignore
+      const lintResult = genericRules[rule.name](value, rule.conditions);
+      if (!lintResult) {
+        report({
+          message: rule.description,
+          location: location.key(),
+        });
+      }
+    }
+  }
+}
 
 export const Enforcements:any = (opts: any) => {
-  let result = {};
+  let rules = {};
 
   for (let key in opts) {
     const enforcement = opts[key];
 
     if (enforcement.on) {
-      // get props and rules:
-      const onProps = Array.isArray(enforcement.on) ? enforcement.on : [enforcement.on];
-      const rulesToApply = Object.keys(rules).filter((rule: string) => enforcement[rule] !== undefined)
+      const onProps: Array<string> = Array.isArray(enforcement.on) ? enforcement.on : [enforcement.on];
+      const rulesToApply: Array<Rule> = Object.keys(genericRules).filter((rule: string) => enforcement[rule] !== undefined)
         .map((rule: string) => ({name: rule, conditions: enforcement[rule], description: enforcement.description}));
-      const hasMutuallyRule = !!(enforcement.mutuallyExclusive || enforcement.mutuallyRequired);
 
        // form rule for each property:
       onProps.forEach((onProp: string) => {
         const parts = onProp.split('.');
         if (parts.length < 2) return; // property should have parent node
 
-        const lastProp: string | null = hasMutuallyRule ? null : parts.pop() as string; // property on which we will do the lint
-        const lastNode: string = parts.pop() as string; // node that have this property
-
         if (parts.length === 0) {
-          // @ts-ignore
           rulesToApply.forEach(rule => {
-            result = {
-              ...result,
-              ...{
-                [lastNode]: function(node: any, { report, location }: any) {
-                  const value = hasMutuallyRule ? node : node[lastProp as string];
-                  // @ts-ignore
-                  const lintResult = rules[rule.name](value, rule.conditions);
-                  if (!lintResult) {
-                    report({
-                      message: rule.description,
-                      location: location.key(),
-                    });
-                  }
-                }
-              }
+            rules = {
+              ...rules,
+              ...formRule(rule, parts, enforcement)
             }
           })
         } else {
-          // @ts-ignore
           rulesToApply.forEach(rule => {
-            const __res = parts.reverse().reduce((res, key, index) => {
-              if (index === 0) {
-                // @ts-ignore
-                res = {[lastNode]: function(node: any, { report, location }: any) {
-                    const value = hasMutuallyRule ? node : node[lastProp as string];
-                    // @ts-ignore
-                    const lintResult = rules[rule.name](value, rule.conditions);
-                    if (!lintResult) {
-                      report({
-                        message: rule.description,
-                        location: location.key(),
-                      });
-                    }
-                  }}
-              }
-              return {[key]: res}
-            }, {});
-            result = {
-              ...result,
-              ...__res
+            rules = {
+              ...rules,
+              ...parts.reverse().reduce((res, key, index) => {
+                if (index === 0) {
+                  res = formRule(rule, parts, enforcement);
+                }
+                return {[key]: res}
+              }, {})
             }
           })
-         }
-       });
+        }
+      });
      }
    }
 
-  return result;
+  return rules;
 };
