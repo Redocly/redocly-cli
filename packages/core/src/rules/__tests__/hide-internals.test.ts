731ad4663b9a69add7a850b0895d6b50b4ef90a4
Andriy Leliv <andrii.leliv@gmail.com>
2021-12-29 12:54:47 +0200

    chore: hide-internals decorator tests

--- packages/core/src/rules/__tests__/hide-internals.test.ts
+++ packages/core/src/rules/__tests__/hide-internals.test.ts
@@ -54,4 +54,81 @@ describe('oas3 hide-internals', () => {
     `
     );
   });
+
+  it('should clean types: Server, Operation, Parameter, PathItem, Example', async () => {
+    const testDoc = parseYamlToDocument(
+      outdent`
+        openapi: 3.1.0
+        servers:
+          - url: //petstore.swagger.io/v2
+            description: Default server
+            x-internal: true
+        paths:
+          /pet:
+            get:
+              x-internal: true
+              operationId: getPet
+              parameters:
+                - $ref: '#/components/parameters/x'
+            put:
+              parameters:
+                - name: Accept-Language
+                  x-internal: true
+                  in: header
+                  example: en-US
+                  required: false
+                - name: cookieParam
+                  x-internal: true
+                  in: cookie
+                  description: Some cookie
+                  required: true
+          /admin:
+            x-internal: true
+            post:
+              parameters:
+                - $ref: '#/components/parameters/y'
+          /store/order:
+            post:
+              operationId: placeOrder
+              responses:
+                '200':
+                  description: successful operation
+                  content:
+                    application/json:
+                      examples:
+                        response:
+                          x-internal: true
+                          value: OK
+        components:
+          parameters:
+            x:
+              name: x
+            y:
+              name: y
+      `);
+      const { bundle: res } = await bundleDocument({
+        document: testDoc,
+        externalRefResolver: new BaseResolver(),
+        config: makeConfig({}, { 'hide-internals': 'on' })
+      });
+      expect(res.parsed).toMatchInlineSnapshot(
+      `
+      openapi: 3.1.0
+      paths:
+        /store/order:
+          post:
+            operationId: placeOrder
+            responses:
+              '200':
+                description: successful operation
+      components:
+        parameters:
+          x:
+            name: x
+          'y':
+            name: 'y'
+
+      `
+      );
+  });
 });
