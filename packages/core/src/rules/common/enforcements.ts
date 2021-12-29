0553f55124c317ff7302825eeac87db757e6be35
Oleksii <oleksii.horbachevskyi@redocly.com>
2021-12-29 23:20:23 +0200

    resolve an issue with merging visitors

--- packages/core/src/rules/common/enforcements.ts
+++ packages/core/src/rules/common/enforcements.ts
@@ -8,15 +8,19 @@ type Rule = {
   description: string
 }
 
-const formRule = (rule: Rule, lastProp: string, lastNode: string, hasMutuallyRule: boolean) => {
+const formRule = (lastNodeName: string, propsToRules:  {[key: string]: Array<Rule>}) => {
   return {
-    [lastNode]: function(node: any, { report, location }: UserContext) {
-      const value = hasMutuallyRule ? node : node[lastProp];
-      const lintResult = (genericRules as {[key: string]: any})[rule.name](value, rule.conditions);
-      if (!lintResult) {
-        report({
-          message: rule.description,
-          location: location.key(),
+    [lastNodeName]: function(node: any, { report, location }: UserContext) {
+      for (let prop in propsToRules) {
+        propsToRules[prop].forEach((rule: Rule) => {
+          const value = prop === '__all' ? node : node[prop];
+          const lintResult = (genericRules as {[key: string]: any})[rule.name](value, rule.conditions);
+          if (!lintResult) {
+            report({
+              message: rule.description,
+              location: location.key(),
+            });
+          }
         });
       }
     }
@@ -24,7 +28,8 @@ const formRule = (rule: Rule, lastProp: string, lastNode: string, hasMutuallyRul
 }
 
 export const Enforcements:  Oas3Rule | Oas2Rule = (opts: any) => {
-  let rules = {};
+  let visitors = {};
+  let rulesMap: {[key: string]: any} = {};
 
   for (let key in opts) {
     const enforcement = opts[key];
@@ -39,32 +44,30 @@ export const Enforcements:  Oas3Rule | Oas2Rule = (opts: any) => {
         const parts = onProp.split('.');
         if (parts.length === 1 && !hasMutuallyRule) return; // property should have parent node
 
-        const lastProp: string = hasMutuallyRule ? '' : parts.pop() as string; // property on which we will do the lint
-        const lastNode: string = parts.pop() as string; // node that have this property
-
-        if (parts.length === 0) {
-          rulesToApply.forEach(rule => {
-            rules = {
-              ...rules,
-              ...formRule(rule, lastProp, lastNode, hasMutuallyRule)
-            }
-          })
-        } else {
-          rulesToApply.forEach(rule => {
-            rules = {
-              ...rules,
-              ...parts.reverse().reduce((res, key, index) => {
-                if (index === 0) {
-                  res = formRule(rule, lastProp, lastNode, hasMutuallyRule);
-                }
-                return {[key]: res}
-              }, {})
-            }
-          })
+        const lastProp: string = hasMutuallyRule ? '__all' : parts.pop() as string; // property on which we will do the lint
+        const key = parts.join('.');
+        if (!rulesMap[key]) {
+          rulesMap[key] = {};
         }
+        rulesMap[key][lastProp] = rulesToApply;
       });
     }
   }
 
-  return rules;
+  for (let path in rulesMap) {
+    const pathParts = path.split('.');
+    const lastNode = pathParts.pop() as string;
+    let visitor = pathParts.reverse().reduce((res, key, index) => {
+      if (index === 0) {
+        res = formRule(lastNode, rulesMap[path]);
+      }
+      return {[key]: res}
+    }, {});
+
+    visitors = {
+      ...visitors,
+      ...visitor
+    }
+  }
+  return visitors;
 };
