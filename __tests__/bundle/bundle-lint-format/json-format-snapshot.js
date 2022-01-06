9112f645ccc53e46fa205f10d23ed338c1f24989
Roman Marshevskyy <marshevskyy@gmail.com>
2022-01-06 13:13:58 +0200

    chore: v1.0.0-beta.77 (#497)

--- __tests__/bundle/bundle-lint-format/json-format-snapshot.js
+++ __tests__/bundle/bundle-lint-format/json-format-snapshot.js
@@ -7,7 +7,7 @@ exports[`E2E bundle lint format bundle lint: should be formatted by format: json
     "warnings": 0,
     "ignored": 0
   },
-  "version": "1.0.0-beta.76",
+  "version": "1.0.0-beta.77",
   "problems": [
     {
       "ruleId": "spec",
@@ -31,7 +31,7 @@ exports[`E2E bundle lint format bundle lint: should be formatted by format: json
     "warnings": 0,
     "ignored": 0
   },
-  "version": "1.0.0-beta.76",
+  "version": "1.0.0-beta.77",
   "problems": []
 }
 No configurations were defined in extends -- using built in recommended configuration by default.
