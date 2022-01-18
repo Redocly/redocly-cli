c0798f2f79147514c823d9c4bebd447b4f5bf231
Andriy Leliv <andrii.leliv@gmail.com>
2022-01-18 15:51:10 +0200

    chore: split running tests for cli & core

---
+++ packages/cli/__mocks__/@redocly/openapi-core.ts
@@ -0,0 +1,25 @@
+export const __redoclyClient = {
+  isAuthorizedWithRedocly: jest.fn().mockResolvedValue(true),
+  isAuthorizedWithRedoclyByRegion: jest.fn().mockResolvedValue(true),
+  login: jest.fn(),
+  registryApi: {
+    setAccessTokens: jest.fn(),
+    authStatus: jest.fn(),
+    prepareFileUpload: jest.fn().mockResolvedValue({
+      signedUploadUrl: 'signedUploadUrl',
+      filePath: 'filePath',
+    }),
+    pushApi: jest.fn(),
+  },
+};
+
+export const RedoclyClient = jest.fn(() => __redoclyClient);
+export const loadConfig = jest.fn(() => ({
+  configFile: null,
+  lint: { skipRules: jest.fn(), skipPreprocessors: jest.fn(), skipDecorators: jest.fn() },
+}));
+export const lint = jest.fn();
+export const bundle = jest.fn(() => ({ bundle: { parsed: null }, problems: null }));
+export const getTotals = jest.fn(() => ({ errors: 0 }));
+export const formatProblems = jest.fn();
+export const slash = jest.fn();
