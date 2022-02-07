f00f1a28052ac44b6d6d303df22eadbebae49c32
Andriy Leliv <andrii.leliv@gmail.com>
2022-02-07 23:01:42 +0200

    feat: config format transformer

--- packages/core/src/config/load.ts
+++ packages/core/src/config/load.ts
@@ -2,22 +2,11 @@ import * as fs from 'fs';
 import { RedoclyClient } from '../redocly';
 import { loadYaml } from '../utils';
 import { Config, DOMAINS, RawConfig, Region } from './config';
-
 import { defaultPlugin } from './builtIn';
 
 export async function loadConfig(configPath?: string, customExtends?: string[]): Promise<Config> {
-  if (configPath === undefined) {
-    configPath = findConfig();
-  }
-  let rawConfig: RawConfig = {};
+  const rawConfig = await getConfig(configPath);
 
-  if (configPath !== undefined) {
-    try {
-      rawConfig = (await loadYaml(configPath)) as RawConfig;
-    } catch (e) {
-      throw new Error(`Error parsing config file at \`${configPath}\`: ${e.message}`);
-    }
-  }
   if (customExtends !== undefined) {
     rawConfig.lint = rawConfig.lint || {};
     rawConfig.lint.extends = customExtends;
@@ -59,6 +48,38 @@ export async function loadConfig(configPath?: string, customExtends?: string[]):
     },
     configPath,
   );
+}
+
+function configTransformer(rawConfig: RawConfig) {
+  if (!rawConfig.apiDefinitions) return rawConfig;
+
+  const config = {} as any;
+  const keysToChange = ['apiDefinitions', 'referenceDocs'];
+
+  for (const [key, value] of Object.entries(rawConfig)) {
+    if (key === 'apiDefinitions') {
+      config.apis = {};
+      for (const [apiName, apiPath] of Object.entries(value)) {
+        config.apis[apiName+'@latest'] = { 'root': apiPath };
+      }
+    }
+    if (key === 'referenceDocs') { config['features.openapi'] = value; }
+    if (!keysToChange.includes(key)) { config[key] = value; }
+  }
+  return config;
+}
+
+export async function getConfig(path?: string) {
+  let rawConfig: RawConfig = {};
+  const configPath = path || findConfig();
+  if (configPath) {
+    try {
+      rawConfig = (await loadYaml(configPath)) as RawConfig;
+    } catch (e) {
+      throw new Error(`Error parsing config file at \`${configPath}\`: ${e.message}`);
+    }
+  }
+  return configTransformer(rawConfig);
 }
 
 function findConfig() {
