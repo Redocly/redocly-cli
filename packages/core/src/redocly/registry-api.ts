bbc34c70b05be5365bd07f714162a542427d2768
tatomyr <andrew.tatomyr@redocly.com>
2022-02-11 12:43:07 +0200

    fix: revert using https-proxy-agent #555

--- packages/core/src/redocly/registry-api.ts
+++ packages/core/src/redocly/registry-api.ts
@@ -3,7 +3,6 @@ import { RegistryApiTypes } from './registry-api-types';
 import { AccessTokens, Region, DEFAULT_REGION, DOMAINS } from '../config/config';
 import { isNotEmptyObject } from '../utils';
 const version = require('../../package.json').version;
-const HttpsProxyAgent = require('https-proxy-agent');
 
 export class RegistryApi {
   constructor(private accessTokens: AccessTokens, private region: Region) {}
@@ -24,11 +23,9 @@ export class RegistryApi {
   private async request(path = '', options: RequestInit = {}, region?: Region) {
     const headers = Object.assign({}, options.headers || {}, { 'x-redocly-cli-version': version });
     if (!headers.hasOwnProperty('authorization')) { throw new Error('Unauthorized'); }
-    const proxy = process.env.REDOCLY_PROXY;
-    const agent = proxy ? new HttpsProxyAgent(proxy) : undefined;
     const response = await fetch(
       `${this.getBaseUrl(region)}${path}`,
-      Object.assign({}, options, { headers, agent }),
+      Object.assign({}, options, { headers }),
     );
     if (response.status === 401) { throw new Error('Unauthorized'); }
     if (response.status === 404) {
