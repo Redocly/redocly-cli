4a5ebf708be66441962a9dbef3e0a52d9a61f075
Andrew Tatomyr <andrewtatomyr@Andrews-MacBook-Pro.local>
2022-03-03 15:02:32 +0100

    chore: update tests

---
+++ __tests__/lint/info-contact--deprecated-format/snapshot.js
@@ -0,0 +1,12 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`E2E lint info-contact--deprecated-format 1`] = `
+The apiDefinitions field is deprecated. Use apis instead.
+
+validating /openapi.yaml...
+/openapi.yaml: validated in <test>ms
+
+Woohoo! Your OpenAPI definition is valid. 🎉
+
+
+`;
