c5f492eb3da36be279657141469d3fcee1cc64b6
Andrew Tatomyr <andrew.tatomyr@gmail.com>
2022-03-16 16:58:49 +0100

    Update packages/core/src/types/redocly-yaml.ts

--- packages/core/src/types/redocly-yaml.ts
+++ packages/core/src/types/redocly-yaml.ts
@@ -10,7 +10,7 @@ const ConfigRoot: NodeType = {
     },
     lint: 'ConfigLint',
     referenceDocs: 'ConfigReferenceDocs',
-    mockServer: 'ConfigMockServer',
+    'features.mockServer': 'ConfigMockServer',
   },
 };
 
