a6c4f15e10d839fa96d3f7e6adb88405a1c48775
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2022-06-03 15:30:24 +0200

    chore: coverage down

--- packages/core/src/config/__tests__/resolve-plugins.test.ts
+++ packages/core/src/config/__tests__/resolve-plugins.test.ts
@@ -11,17 +11,17 @@ describe('resolving a plugin', () => {
     expect(plugin.rules?.oas3).toHaveProperty('test-plugin/openid-connect-url-well-known');
   });
 
-  // it('should prefix preprocessor names with the plugin id', async () => {
-  //   const config = await loadConfig(configPath);
-  //   const plugin = config.lint.plugins[0];
+  it('should prefix preprocessor names with the plugin id', async () => {
+    const config = await loadConfig(configPath);
+    const plugin = config.lint.plugins[0];
 
-  //   expect(plugin.preprocessors?.oas2).toHaveProperty('test-plugin/description-preprocessor');
-  // });
+    expect(plugin.preprocessors?.oas2).toHaveProperty('test-plugin/description-preprocessor');
+  });
 
-  // it('should prefix decorator names with the plugin id', async () => {
-  //   const config = await loadConfig(configPath);
-  //   const plugin = config.lint.plugins[0];
+  it('should prefix decorator names with the plugin id', async () => {
+    const config = await loadConfig(configPath);
+    const plugin = config.lint.plugins[0];
 
-  //   expect(plugin.decorators?.oas3).toHaveProperty('test-plugin/inject-x-stats');
-  // });
+    expect(plugin.decorators?.oas3).toHaveProperty('test-plugin/inject-x-stats');
+  });
 });
