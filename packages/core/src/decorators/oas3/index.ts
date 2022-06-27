70c4d952f5585c17d667dbe53b0ab146fd9156a7
Ihor Karpiuk <106662428+IgorKarpiuk@users.noreply.github.com>
2022-06-27 10:54:26 +0300

    feat: add new decorators filter-in and filter-out (#736)
    * feat: add new decorators filter-in and filter-out
    
    * chore: remove unused parameters for filter tests
    
    * chore: rename parameter and add new test

--- packages/core/src/decorators/oas3/index.ts
+++ packages/core/src/decorators/oas3/index.ts
@@ -4,11 +4,15 @@ import { OperationDescriptionOverride } from '../common/operation-description-ov
 import { TagDescriptionOverride } from '../common/tag-description-override';
 import { InfoDescriptionOverride } from '../common/info-description-override';
 import { RemoveXInternal } from '../common/remove-x-internal';
+import { FilterIn } from '../common/filters/filter-in';
+import { FilterOut } from '../common/filters/filter-out';
 
 export const decorators = {
   'registry-dependencies': RegistryDependencies as Oas3Decorator,
   'operation-description-override': OperationDescriptionOverride as Oas3Decorator,
   'tag-description-override': TagDescriptionOverride as Oas3Decorator,
   'info-description-override': InfoDescriptionOverride as Oas3Decorator,
-  'remove-x-internal': RemoveXInternal as Oas3Decorator
+  'remove-x-internal': RemoveXInternal as Oas3Decorator,
+  'filter-in': FilterIn as Oas3Decorator,
+  'filter-out': FilterOut as Oas3Decorator,
 };
