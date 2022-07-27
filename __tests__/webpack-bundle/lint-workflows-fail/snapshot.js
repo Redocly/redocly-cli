a620f3a9f3f02d86f7d82c2121b9afe17a490411
SmoliyY <yevhen.smoliy@gmail.com>
2022-07-27 18:51:03 +0300

    chore: more e2e test added

---
+++ __tests__/webpack-bundle/lint-workflows-fail/snapshot.js
@@ -0,0 +1,16 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`webpack-bundle test lint-workflows-fail 1`] = `
+
+validating ./openapi.yaml...
+openapi.yaml:
+  72:5  error    operation-summary      Operation object should contain \`summary\` field.
+  72:5  warning  operation-operationId  Operation object should contain \`operationId\` field.
+
+./openapi.yaml: validated in <test>ms
+
+❌ Validation failed with 1 error and 1 warning.
+run \`openapi lint --generate-ignore-file\` to add all problems to the ignore file.
+
+
+`;
