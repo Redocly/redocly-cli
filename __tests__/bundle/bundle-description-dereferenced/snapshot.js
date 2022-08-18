9ef1c332bd1c2a90aead8822b7dbc2a6aa65c261
iosypov <ivan.osypov@redocly.com>
2022-08-18 15:43:41 +0300

    Change test.yaml

--- __tests__/bundle/bundle-description-dereferenced/snapshot.js
+++ __tests__/bundle/bundle-description-dereferenced/snapshot.js
@@ -2,21 +2,6 @@
 
 exports[`E2E bundle bundle-description-dereferenced 1`] = `
 openapi: 3.1.0
-info:
-  version: 1.0.0
-  title: Example.com
-  termsOfService: https://example.com/terms/
-  contact:
-    email: contact@example.com
-    url: http://example.com/contact
-  license:
-    name: Apache 2.0
-    url: http://www.apache.org/licenses/LICENSE-2.0.html
-  description: |
-    This is an **example** API to demonstrate features of the OpenAPI
-    specification.
-servers:
-  - url: https://example.com/api/v1
 paths:
   /users:
     post:
@@ -31,16 +16,14 @@ paths:
         content:
           application/json:
             schema:
-              description: Names and Numbers (specific)
-              $ref: '#/components/schemas/NamesAndNumbers'
+              description: Names (specific)
+              $ref: '#/components/schemas/Names'
         required: true
 components:
   schemas:
     Name:
       type: string
       description: Generic Name.
-    Number:
-      type: integer
     Names:
       type: object
       description: names description
@@ -51,35 +34,59 @@ components:
         otherName:
           $ref: '#/components/schemas/Name'
           description: Other name (specific).
-    Numbers:
-      type: object
-      description: numbers description
-      properties:
-        oneNumber:
-          $ref: '#/components/schemas/Number'
-          description: One number (specific)
-        otherNumber:
-          $ref: '#/components/schemas/Number'
-          description: Other number (specific)
-    NamesAndNumbers:
-      type: object
-      description: names and numbers description
-      properties:
-        names:
-          $ref: '#/components/schemas/Names'
-        numbers:
-          $ref: '#/components/schemas/Numbers'
 
 No configurations were defined in extends -- using built in recommended configuration by default.
 Warning! This default behavior is going to be deprecated soon.
 
 test.yaml:
-  23:7  warning  operation-4xx-response  Operation must have at least one \`4xx\` response.
+  1:1  error    spec  The field \`info\` must be present on this level.
 
-Woohoo! Your OpenAPI definitions are valid. 🎉
-You have 1 warning.
+< ... 2 more problems hidden > increase with \`--max-problems N\`
+❌ Validation failed with 2 errors and 1 warning.
+run \`openapi lint --generate-ignore-file\` to add all problems to the ignore file.
 
 bundling ./test.yaml...
 📦 Created a bundle for ./test.yaml at stdout <test>ms.
+
+`;
+
+exports[`E2E bundle with option: dereferenced description should not be from $ref 1`] = `
+openapi: 3.1.0
+paths:
+  /users:
+    post:
+      summary: user
+      description: User.
+      operationId: createUser
+      responses:
+        '200':
+          description: OK
+      requestBody:
+        description: Updated user object
+        content:
+          application/json:
+            schema:
+              description: Names (specific)
+              type: object
+              properties: &ref_0
+                oneName:
+                  description: One name (specific).
+                  type: string
+                otherName:
+                  description: Other name (specific).
+                  type: string
+        required: true
+components:
+  schemas:
+    Name:
+      type: string
+      description: Generic Name.
+    Names:
+      type: object
+      description: names description
+      properties: *ref_0
+
+bundling test.yaml...
+📦 Created a bundle for test.yaml at stdout <test>ms.
 
 `;
