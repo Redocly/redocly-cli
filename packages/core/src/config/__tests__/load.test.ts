90b9104db830487fed901682929ebb956a2c681a
Roman Sainchuk <albuman32@gmail.com>
2022-08-19 18:33:09 +0300

    test: add tests to createConfig

--- packages/core/src/config/__tests__/load.test.ts
+++ packages/core/src/config/__tests__/load.test.ts
@@ -1,5 +1,7 @@
-import { loadConfig, findConfig, getConfig } from '../load';
+import { loadConfig, findConfig, getConfig, createConfig } from '../load';
 import { RedoclyClient } from '../../redocly';
+import { RuleConfig, RawConfig } from './../types';
+import { Config } from '../config';
 
 const fs = require('fs');
 const path = require('path');
@@ -87,3 +89,81 @@ describe('getConfig', () => {
     expect(getConfig()).toEqual(Promise.resolve({}));
   });
 });
+
+describe('createConfig', () => {
+  it('should create config from string', async () => {
+    const config = await createConfig(`
+      styleguide:
+        extends:
+          - recommended
+        rules:
+          info-license: off
+    `);
+
+    const overridenRules: Record<string, RuleConfig> = { 'info-license': 'off' };
+
+    verifyExtendedConfig(config, {
+      extendsRuleSet: 'recommended',
+      overridenRules,
+    });
+  });
+
+  it('should create config from object', async () => {
+    const rawConfig: RawConfig = {
+      styleguide: {
+        extends: ['minimal'],
+        rules: {
+          'info-license': 'off',
+          'tag-description': 'off',
+          'operation-2xx-response': 'off'
+        },
+      },
+    };
+    const config = await createConfig(rawConfig);
+
+    verifyExtendedConfig(config, {
+      extendsRuleSet: 'minimal',
+      overridenRules: rawConfig.styleguide!.rules as Record<string, RuleConfig>,
+    });
+  });
+});
+
+function verifyExtendedConfig(
+  config: Config,
+  {
+    extendsRuleSet,
+    overridenRules,
+  }: { extendsRuleSet: string; overridenRules: Record<string, RuleConfig> }
+) {
+  const defaultPlugin = config.styleguide.plugins.find((plugin) => plugin.id === '');
+  expect(defaultPlugin).toBeDefined();
+
+  const recommendedRules = defaultPlugin?.configs?.[extendsRuleSet];
+  expect(recommendedRules).toBeDefined();
+
+  verifyOasRules(config.styleguide.rules.oas2, overridenRules, recommendedRules?.rules || {});
+  verifyOasRules(
+    config.styleguide.rules.oas3_0,
+    overridenRules,
+    Object.assign({}, recommendedRules?.rules, recommendedRules?.oas3_0Rules)
+  );
+  verifyOasRules(
+    config.styleguide.rules.oas3_1,
+    overridenRules,
+    Object.assign({}, recommendedRules?.rules, recommendedRules?.oas3_1Rules)
+  );
+}
+
+function verifyOasRules(
+  styleguideRuleset: Record<string, RuleConfig>,
+  overridenRules: Record<string, RuleConfig>,
+  ruleSet: Record<string, RuleConfig>
+) {
+  Object.entries(styleguideRuleset).forEach(([ruleName, ruleValue]) => {
+    if (ruleName in overridenRules) {
+      expect(ruleValue).toBe(overridenRules[ruleName]);
+    } else {
+      expect(ruleValue).toBe(ruleSet[ruleName]);
+    }
+  });
+}
