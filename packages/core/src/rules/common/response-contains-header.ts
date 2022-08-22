2359fdb43fee014701d66b627701e150d79e11b1
Jeremy Fiel <jeremyrfiel@gmail.com>
2022-08-22 17:11:34 -0400

    fix: force response headers toLowerCase per RFC2616#4.2
        - all headers are case-insensitive
    
        #824

--- packages/core/src/rules/common/response-contains-header.ts
+++ packages/core/src/rules/common/response-contains-header.ts
@@ -16,9 +16,9 @@ export const ResponseContainsHeader: Oas3Rule | Oas2Rule = (options) => {
             names[getMatchingStatusCodeRange(key).toLowerCase()] ||
             [];
           for (const expectedHeader of expectedHeaders) {
-            if (!response.headers?.[expectedHeader]) {
+            if (!response.headers?.[expectedHeader.toLowerCase()]) {
               report({
-                message: `Response object must contain a "${expectedHeader}" header.`,
+                message: `Response object must contain a "${expectedHeader.toLowerCase()}" header.`,
                 location: location.child('headers').key(),
               });
             }
