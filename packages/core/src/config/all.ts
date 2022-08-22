3746ea062fb92f37919f4e33f384f49ad5c2e922
iosypov <ivan.osypov@redocly.com>
2022-08-22 11:16:01 +0300

    Add rule to all rules

--- packages/core/src/config/all.ts
+++ packages/core/src/config/all.ts
@@ -19,6 +19,7 @@ export default {
     'operation-description': 'error',
     'operation-2xx-response': 'error',
     'operation-4xx-response': 'error',
+    'operation-4xx-problem-details-rfc7807': 'error',
     assertions: 'error',
     'operation-operationId': 'error',
     'operation-summary': 'error',
