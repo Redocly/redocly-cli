0804d2a28f4b1ff39073b147b92c02c70dcb0cac
iosypov <ivan.osypov@redocly.com>
2022-09-05 17:28:32 +0300

    Update test

--- __tests__/lint/assertions-custom-function-options/plugin.js
+++ __tests__/lint/assertions-custom-function-options/plugin.js
@@ -2,14 +2,14 @@ module.exports = {
   id: 'local',
   assertions: {
     checkWordsStarts: (value, opts, location) => {
-      const regexp = new RegExp(`^${opts.words.join("|")}`)
+      const regexp = new RegExp(`^${opts.words.join('|')}`);
       if (regexp.test(value)) {
         return { isValid: true };
       }
       return { isValid: false, location };
     },
     checkWordsCount: (value, opts, location) => {
-      const words = value.split(" ");
+      const words = value.split(' ');
       if (words.length >= opts.min) {
         return { isValid: true };
       }
