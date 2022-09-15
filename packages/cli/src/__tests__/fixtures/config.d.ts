1630c6e7f432d98c8e896deeb163c00eab48a953
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2022-09-15 16:50:27 +0300

    chore: remove tests from CLI npm package

---
+++ packages/cli/src/__tests__/fixtures/config.d.ts
@@ -0,0 +1,11 @@
+export declare const ConfigFixture: {
+  configFile: null;
+  styleguide: {
+      addIgnore: any;
+      skipRules: any;
+      skipPreprocessors: any;
+      saveIgnore: any;
+      skipDecorators: any;
+      ignore: null;
+  };
+};
