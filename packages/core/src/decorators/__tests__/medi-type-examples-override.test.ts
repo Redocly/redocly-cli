66178cd708768d8426b5c3f434c9162aa6bd885b
igor <Ihor.karpiuk@redocly.com>
2022-12-05 12:08:02 +0200

    feat: resolve components references

--- packages/core/src/decorators/__tests__/medi-type-examples-override.test.ts
+++ packages/core/src/decorators/__tests__/medi-type-examples-override.test.ts
@@ -198,6 +198,192 @@ describe('oas3 media-type-examples-override', () => {
     `);
   });
 
+  it('should override response and request examples with refs', async () => {
+    const testDocument = parseYamlToDocument(
+      outdent`
+            openapi: 3.0.0
+            paths:
+              /pet:
+                get:
+                  operationId: getUserById
+                  responses:
+                    '200':
+                      $ref: '#/components/responses/okay200'
+                  requestBody: 
+                    $ref: '#/components/requestBodies/testRequest'
+            components:
+              requestBodies:
+                testRequest:
+                  content:
+                    application/json:
+                      example:
+                        def:
+                          value:
+                            a: test123
+              responses:
+                okay200:
+                  description: json
+                  content:
+                    application/json:
+                      example:
+                        def:
+                          value:
+                            a: t
+                            b: 3                          
+    `
+    );
+
+    const { bundle: res } = await bundleDocument({
+      document: testDocument,
+      externalRefResolver: new BaseResolver(),
+      config: await makeConfig(
+        {},
+        {
+          'media-type-examples-override': {
+            operationIds: {
+              getUserById: {
+                request: {
+                  'application/json':
+                    'packages/core/src/decorators/__tests__/resources/request.yaml',
+                },
+                responses: {
+                  '200': 'packages/core/src/decorators/__tests__/resources/response.yaml',
+                },
+              },
+            },
+          },
+        }
+      ),
+    });
+
+    expect(res.parsed).toMatchInlineSnapshot(`
+      openapi: 3.0.0
+      paths:
+        /pet:
+          get:
+            operationId: getUserById
+            responses:
+              '200':
+                description: json
+                content:
+                  application/json:
+                    example:
+                      def:
+                        value:
+                          name: test response name
+            requestBody:
+              content:
+                application/json:
+                  example:
+                    def:
+                      value:
+                        b: from external file
+      components:
+        requestBodies:
+          testRequest:
+            content:
+              application/json:
+                example:
+                  def:
+                    value:
+                      a: test123
+        responses:
+          okay200:
+            description: json
+            content:
+              application/json:
+                example:
+                  def:
+                    value:
+                      a: t
+                      b: 3
+
+    `);
+  });
+
+  it('should override example with ref', async () => {
+    const testDocument = parseYamlToDocument(
+      outdent`
+            openapi: 3.0.0
+            paths:
+              /pet:
+                get:
+                  operationId: getUserById
+                  responses:
+                    '400':
+                      description: bad request
+                      content:
+                        application/json:
+                          example:
+                            $ref: '#/components/examples/testExample'
+                  requestBody: 
+                    content:
+                      application/json:
+                        example:
+                          obj:
+                            $ref: '#/components/examples/testExample'
+            components:
+              examples:
+                testExample:
+                  value:
+                    test: 1                          
+    `
+    );
+
+    const { bundle: res } = await bundleDocument({
+      document: testDocument,
+      externalRefResolver: new BaseResolver(),
+      config: await makeConfig(
+        {},
+        {
+          'media-type-examples-override': {
+            operationIds: {
+              getUserById: {
+                request: {
+                  'application/json':
+                    'packages/core/src/decorators/__tests__/resources/request.yaml',
+                },
+                responses: {
+                  '400': 'packages/core/src/decorators/__tests__/resources/response.yaml',
+                },
+              },
+            },
+          },
+        }
+      ),
+    });
+
+    expect(res.parsed).toMatchInlineSnapshot(`
+      openapi: 3.0.0
+      paths:
+        /pet:
+          get:
+            operationId: getUserById
+            responses:
+              '400':
+                description: bad request
+                content:
+                  application/json:
+                    example:
+                      def:
+                        value:
+                          name: test response name
+            requestBody:
+              content:
+                application/json:
+                  example:
+                    def:
+                      value:
+                        b: from external file
+      components:
+        examples:
+          testExample:
+            value:
+              test: 1
+
+    `);
+  });
+
   it('should not override the example', async () => {
     const testDocument = parseYamlToDocument(
       outdent`
--- packages/core/src/decorators/common/media-type-examples-override.ts
+++ packages/core/src/decorators/common/media-type-examples-override.ts
@@ -1,11 +1,22 @@
 import { Oas3Decorator } from '../../visitors';
-import { Oas3Operation, Oas3RequestBody } from '../../typings/openapi';
+import { Oas3Operation, Oas3RequestBody, Oas3Response } from '../../typings/openapi';
 import { readAndParseFileSync } from '../../utils';
+import { isRef } from '../../ref-utils';
+import { ResolveFn, UserContext } from '../../walk';
 
 export const MediaTypeExamplesOverride: Oas3Decorator = ({ operationIds }) => {
+  function checkAndResolveRef<T>(node: any, resolver: ResolveFn): T | undefined {
+    if (!isRef(node)) {
+      return node;
+    }
+
+    const resolved = resolver<T>(node);
+    return resolved.error ? undefined : JSON.parse(JSON.stringify(resolved.node));
+  }
+
   return {
     Operation: {
-      leave(operation: Oas3Operation) {
+      enter(operation: Oas3Operation, ctx: UserContext) {
         const operationId = operation.operationId;
 
         if (!operationId) {
@@ -14,24 +25,46 @@ export const MediaTypeExamplesOverride: Oas3Decorator = ({ operationIds }) => {
 
         const properties = operationIds[operationId];
 
+        if (!properties) {
+          return;
+        }
+
         if (properties.responses && operation.responses) {
           for (const responseCode of Object.keys(properties.responses)) {
-            if (!operation.responses[responseCode] || !operation.responses[responseCode]?.content) {
+            const resolvedResponse = checkAndResolveRef<Oas3Response>(
+              operation.responses[responseCode],
+              ctx.resolve
+            );
+
+            if (!resolvedResponse || !resolvedResponse?.content) {
               continue;
             }
 
-            Object.values(operation.responses[responseCode].content!).forEach((mimeType) => {
+            Object.values(resolvedResponse.content!).forEach((mimeType) => {
               const exampleField = mimeType.examples ? 'examples' : 'example';
               mimeType[exampleField] = readAndParseFileSync(properties.responses[responseCode]);
             });
+
+            operation.responses[responseCode] = resolvedResponse;
           }
         }
 
         if (properties.request && operation.requestBody) {
+          const resolvedRequest = checkAndResolveRef<Oas3RequestBody>(
+            operation.requestBody,
+            ctx.resolve
+          );
+
+          if (!resolvedRequest || !resolvedRequest?.content) {
+            return;
+          }
+
           Object.keys(properties.request).forEach((mimeType) => {
-            (operation.requestBody as Oas3RequestBody).content[mimeType].example =
-              readAndParseFileSync(properties.request[mimeType]);
+            resolvedRequest.content[mimeType].example = readAndParseFileSync(
+              properties.request[mimeType]
+            );
           });
+          operation.requestBody = resolvedRequest;
         }
       },
     },
