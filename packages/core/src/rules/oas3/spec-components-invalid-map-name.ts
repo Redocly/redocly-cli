9f3d618484a27e1d85ba6097eb4c4130c486ad85
Anton Kozachuk <54616703+AntonKozachuk@users.noreply.github.com>
2022-12-23 17:05:45 +0200

    fix: spec-components-invalid-map-name not applied for all examples (#969)

--- packages/core/src/rules/oas3/spec-components-invalid-map-name.ts
+++ packages/core/src/rules/oas3/spec-components-invalid-map-name.ts
@@ -20,33 +20,54 @@ export const SpecComponentsInvalidMapName: Oas3Rule = () => {
   }
 
   return {
-    Components: {
-      Parameter(_node, { key, report, location }: UserContext) {
-        validateKey(key, report, location, 'parameters');
-      },
-      Response(_node, { key, report, location }: UserContext) {
-        validateKey(key, report, location, 'responses');
-      },
+    NamedSchemas: {
       Schema(_node, { key, report, location }: UserContext) {
         validateKey(key, report, location, 'schemas');
       },
+    },
+    NamedParameters: {
+      Parameter(_node, { key, report, location }: UserContext) {
+        validateKey(key, report, location, 'parameters');
+      },
+    },
+    NamedResponses: {
+      Response(_node, { key, report, location }: UserContext) {
+        validateKey(key, report, location, 'responses');
+      },
+    },
+    NamedExamples: {
       Example(_node, { key, report, location }: UserContext) {
         validateKey(key, report, location, 'examples');
       },
+    },
+    NamedRequestBodies: {
       RequestBody(_node, { key, report, location }: UserContext) {
         validateKey(key, report, location, 'requestBodies');
       },
+    },
+    NamedHeaders: {
       Header(_node, { key, report, location }: UserContext) {
         validateKey(key, report, location, 'headers');
       },
+    },
+    NamedSecuritySchemes: {
       SecurityScheme(_node, { key, report, location }: UserContext) {
-        validateKey(key, report, location, 'securitySchemas');
+        validateKey(key, report, location, 'securitySchemes');
       },
+    },
+    NamedLinks: {
       Link(_node, { key, report, location }: UserContext) {
         validateKey(key, report, location, 'links');
       },
+    },
+    NamedCallbacks: {
       Callback(_node, { key, report, location }: UserContext) {
         validateKey(key, report, location, 'callbacks');
+      },
+    },
+    ExampleMap: {
+      Example(_node, { key, report, location }: UserContext) {
+        validateKey(key, report, location, 'examples');
       },
     },
   };
