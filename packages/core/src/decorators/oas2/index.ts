f99cf0cf16c417dd4ab2bfc4336bd89f24075131
Roman Hotsiy <gotsijroman@gmail.com>
2023-01-25 19:18:11 +0800

    feat: new decorator info-override (#999)

--- packages/core/src/decorators/oas2/index.ts
+++ packages/core/src/decorators/oas2/index.ts
@@ -3,6 +3,7 @@ import { RegistryDependencies } from '../common/registry-dependencies';
 import { OperationDescriptionOverride } from '../common/operation-description-override';
 import { TagDescriptionOverride } from '../common/tag-description-override';
 import { InfoDescriptionOverride } from '../common/info-description-override';
+import { InfoOverride } from '../common/info-override';
 import { RemoveXInternal } from '../common/remove-x-internal';
 import { FilterIn } from '../common/filters/filter-in';
 import { FilterOut } from '../common/filters/filter-out';
@@ -12,6 +13,7 @@ export const decorators = {
   'operation-description-override': OperationDescriptionOverride as Oas2Decorator,
   'tag-description-override': TagDescriptionOverride as Oas2Decorator,
   'info-description-override': InfoDescriptionOverride as Oas2Decorator,
+  'info-override': InfoOverride as Oas2Decorator,
   'remove-x-internal': RemoveXInternal as Oas2Decorator,
   'filter-in': FilterIn as Oas2Decorator,
   'filter-out': FilterOut as Oas2Decorator,
