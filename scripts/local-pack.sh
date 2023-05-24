cce0caf3030c8c24f16669c74263d675a33c1bfb
IhorKarpiuk <ihor.karpiuk@redocly.com>
2023-05-24 18:44:12 +0300

    fix: include core package to cli pack

---
+++ scripts/local-pack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd packages/core && core=$(npm pack | tail -n 1) && mv $core ../../openapi-core.tgz && cd ../../ &&
+
+version=$(cat ./packages/core/package.json | jq '.version' | tr -d '"')
+jq '.dependencies."@redocly/openapi-core" = $packagefile' ./packages/cli/package.json --arg packagefile file:../../$core > package.json.tmp && mv package.json.tmp ./packages/cli/package.json &&
+
+cd packages/cli && cli=$(npm pack | tail -n 1) && mv $cli ../../redocly-cli.tgz
+
+
+
