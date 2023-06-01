096d6fc4cb269afe4b078136e14e248ebb2c207b
IhorKarpiuk <ihor.karpiuk@redocly.com>
2023-06-01 10:38:00 +0300

    fix: update error message

--- packages/core/src/rules/oas3/spec-ref-validation.ts
+++ packages/core/src/rules/oas3/spec-ref-validation.ts
@@ -1,50 +1,29 @@
-import { Oas3Rule, VisitFunction } from '../../visitors';
+import { Oas3Rule } from '../../visitors';
 import { isRef } from '../../ref-utils';
-import { UserContext } from '../../walk';
 
 export const SpecRefValidation: Oas3Rule = () => {
-  function validateNoRef(node: unknown, { report, rawLocation }: UserContext) {
-    if (isRef(node)) {
-      report({
-        message: 'Invalid usage of $ref',
-        location: rawLocation.child('$ref').key(),
-      });
-    }
-  }
-
   const nodesToValidate = [
-    'Tag',
-    'Server',
-    'ServerVariable',
-    'Info',
-    'Contact',
-    'License',
-    'Paths',
-    'Operation',
-    'ExternalDocs',
-    'Encoding',
-    'Responses',
-    'MediaType',
-    'Xml',
-    'SchemaProperties',
-    'DiscriminatorMapping',
-    'Discriminator',
-    'Components',
-    'ImplicitFlow',
-    'PasswordFlow',
-    'ClientCredentials',
-    'AuthorizationCode',
-    'OAuth2Flows',
-    'SpecExtension',
+    'Schema',
+    'Response',
+    'Parameter',
+    'RequestBody',
+    'Example',
+    'Examples',
+    'Header',
+    'SecurityScheme',
+    'Link',
+    'Callback',
+    'PathItem',
   ];
 
-  const ruleObject: Record<string, VisitFunction<unknown>> = {};
-
-  nodesToValidate.forEach((node) => {
-    ruleObject[node] = (_node: unknown, { report, rawNode, rawLocation }: UserContext) => {
-      validateNoRef(rawNode, { report, rawLocation } as UserContext);
-    };
-  });
-
-  return ruleObject;
+  return {
+    any(_node, { report, rawNode, rawLocation, type }) {
+      if (!nodesToValidate.includes(type.name) && isRef(rawNode)) {
+        report({
+          message: 'Field $ref is not expected here.',
+          location: rawLocation.child('$ref').key(),
+        });
+      }
+    },
+  };
 };
