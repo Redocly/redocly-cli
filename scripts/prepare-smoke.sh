be6c707a52e36c03584f0f0b4fb727d2db08f342
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2023-07-08 00:50:50 +0300

    test: common script; styled-components@5.3.3

--- scripts/prepare-smoke.sh
+++ scripts/prepare-smoke.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-bash scripts/local-pack.sh
+npm i
 
-# npm i -g yarn@^1.22.19
+npm run pack:prepare
+
 
 ls ..
 mkdir ../__tmp__
 
 cp ./redocly-cli.tgz ./openapi-core.tgz ./resources/package.json ./resources/openapi.yaml ../__tmp__/
+
+
+# # For yarn
+# cd packages/cli
+# npm pack
+# pwd
+# ls
+# cp ./redocly-cli-1.0.0-beta.130.tgz ../../../__tmp__/redocly-cli-yarn.tgz
+
--- scripts/run-smoke.sh
+++ scripts/run-smoke.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
+echo
+echo "Running smoke test for $1"
+echo "NPM version: $(npm -v)"
+echo "Yarn version: $(yarn --version)"
+echo
 
 cd ../__tmp__
 rm -rf node_modules package-lock.json yarn.lock .yarn redoc-static.html
-
-echo "NPM version:"
-npm -v
-echo
-
 echo "Directory content:"
 ls -a
 echo
 
-npm i redocly-cli.tgz
+# Executing the command provided as the first argument 
+$($1)
 
+# Actual smoke test
 npm run l
 npm run b
 npm run d
-
+if [[ "$(wc -l redoc-static.html)" == "     317 redoc-static.html" ]]; then
+  echo "Built correctly."
+else	
+	echo "Built incorrectly. Received lines:"
+	wc -l redoc-static.html
+	echo "Expected lines: 317."
+  exit 1
+fi
