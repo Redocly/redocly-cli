c5f8641911f56b2e7db2a174474f5399cc501d85
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2023-07-08 02:12:52 +0300

    test: revert to peers

--- scripts/prepare-smoke.sh
+++ scripts/prepare-smoke.sh
@@ -11,12 +11,3 @@ ls ..
 mkdir ../__tmp__
 
 cp ./redocly-cli.tgz ./redocly-cli-clean.tgz ./openapi-core.tgz ./resources/package.json ./resources/openapi.yaml ../__tmp__/
-
-
-# # For yarn
-# cd packages/cli
-# npm pack
-# pwd
-# ls
-# cp ./redocly-cli-1.0.0-beta.130.tgz ../../../__tmp__/redocly-cli-clean.tgz
-
