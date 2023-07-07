f271d7071ed7251ccbc2b8b02bfec32b760ccaea
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2023-07-08 01:33:32 +0300

    test: fix5

--- scripts/prepare-smoke.sh
+++ scripts/prepare-smoke.sh
@@ -2,7 +2,7 @@
 
 npm i
 
-cd packages/cli && cli=$(npm pack | tail -n 1) && mv $cli ../../redocly-cli-clean.tgz # for yarn
+(cd packages/cli && cli=$(npm pack | tail -n 1) && mv $cli ../../redocly-cli-clean.tgz) # for yarn
 
 npm run pack:prepare # for npm
 
@@ -10,7 +10,7 @@ npm run pack:prepare # for npm
 ls ..
 mkdir ../__tmp__
 
-cp ./redocly-cli.tgz ./openapi-core.tgz ./resources/package.json ./resources/openapi.yaml ../__tmp__/
+cp ./redocly-cli.tgz ./redocly-cli-clean.tgz ./openapi-core.tgz ./resources/package.json ./resources/openapi.yaml ../__tmp__/
 
 
 # # For yarn
