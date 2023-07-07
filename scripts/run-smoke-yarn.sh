0be5e92b38778dcc2505061cb691e7fdea7e0939
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2023-07-07 22:54:16 +0300

    test: yarn

---
+++ scripts/run-smoke-yarn.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+cd ../__tmp__
+
+rm -rf node_modules package-lock.json yarn.lock .yarn
+
+echo "Yarn version:"
+yarn --version
+echo
+
+echo "Directory content:"
+ls -a
+echo
+
+yarn install
+
+yarn add ./redocly-cli-yarn.tgz
+
+yarn l
+yarn b
+yarn d
+
