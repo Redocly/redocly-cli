6328eb7774a41399b02d8e72f3995089deec78d6
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2023-07-08 02:06:18 +0300

    test: fail on lint2

--- scripts/run-smoke.sh
+++ scripts/run-smoke.sh
@@ -17,11 +17,11 @@ $($1)
 
 # Actual smoke test
 npm run l
-npm run b
-npm run d
-if [[ "$(wc -l redoc-static.html)" == "317 redoc-static.html" ]]; then
-  echo "Bundled correctly."
-else
-  echo "Bundled incorrectly. Received lines: $(wc -l redoc-static.html) (expected 317 lines in redoc-static.html)."
-  exit 1
-fi
+# npm run b
+# npm run d
+# if [[ "$(wc -l redoc-static.html)" == "317 redoc-static.html" ]]; then
+#   echo "Bundled correctly."
+# else
+#   echo "Bundled incorrectly. Received lines: $(wc -l redoc-static.html) (expected 317 lines in redoc-static.html)."
+#   exit 1
+# fi
