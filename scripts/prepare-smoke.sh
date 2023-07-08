d7b78a84aa159535de282ec149a029648c307852
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2023-07-08 10:18:51 +0300

    change: use centralized job

--- scripts/prepare-smoke.sh
+++ scripts/prepare-smoke.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-npm i
+# npm i # FIXME: delete or uncomment!
 
 (cd packages/cli && cli=$(npm pack | tail -n 1) && mv $cli ../../redocly-cli-clean.tgz) # for yarn
 
 npm run pack:prepare # for npm
 
+echo "Current directory content:"
+ls -a
+echo
+echo "Output directory content:"
+ls -a ..
+echo
 
-ls ..
 mkdir ../__tmp__
 
 cp ./redocly-cli.tgz ./redocly-cli-clean.tgz ./openapi-core.tgz ./resources/package.json ./resources/openapi.yaml ../__tmp__/
