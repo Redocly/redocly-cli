a5009029e5f2928f6253eb813ca507664e51b828
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2023-07-10 13:06:21 +0300

    fix: failing in node 14- and yarn due to unresolved peed dependencies (#1166)
    * chore: set up smoke tests

---
+++ __tests__/smoke/run-smoke.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo
+echo "Running smoke test for command: $1 -> $2"
+echo "NPM version: $(npm -v)"
+echo "Yarn version: $(yarn --version)"
+echo 
+
+set -eo pipefail # Fail on script errors
+
+cd __tests__/smoke
+
+echo "Directory content:"
+ls -a
+echo
+
+# Executing the command provided as the first argument 
+$1
+
+# Actual smoke test - executing the command provided as the second argument
+$2 redocly-version
+$2 redocly-lint
+$2 redocly-bundle
+$2 redocly-build-docs
+# Check for broken styles (related issue: https://github.com/Redocly/redocly-cli/issues/1073)
+if [[ "$(wc -l redoc-static.html)" == "317 redoc-static.html" ]]; then
+  echo "Docs built correctly."
+else
+  echo "Docs built incorrectly. Received lines: $(wc -l redoc-static.html) (expected 317 lines in redoc-static.html)."
+  exit 1
+fi
