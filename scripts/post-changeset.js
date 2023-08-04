04592e1851c83c2bde7e6658c87cac3f3182e1cf
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2023-08-04 17:42:25 +0300

    chore: run prettier

--- scripts/post-changeset.js
+++ scripts/post-changeset.js
@@ -5,6 +5,9 @@ const [, log] = generatedLogs.split('\n## ', 2);
 const mainChangelog = fs.readFileSync('./docs/changelog.md').toString();
 const [date] = new Date().toISOString().split('T');
 const logWithDate = log.replace('\n', ` (${date})\n`);
-const modifiedChangelog = mainChangelog.replace('<!-- do-not-remove -->\n', '<!-- do-not-remove -->\n\n## ' + logWithDate);
+const modifiedChangelog = mainChangelog.replace(
+  '<!-- do-not-remove -->\n',
+  '<!-- do-not-remove -->\n\n## ' + logWithDate
+);
 
 fs.writeFileSync('./docs/changelog.md', modifiedChangelog);
