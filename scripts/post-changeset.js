954fb0237241ea5d098389255405fe30ce717170
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2023-08-07 14:28:50 +0300

    chore: fix release date (#1214)

--- scripts/post-changeset.js
+++ scripts/post-changeset.js
@@ -4,7 +4,7 @@ const generatedLogs = fs.readFileSync('./packages/cli/CHANGELOG.md').toString();
 const [, log] = generatedLogs.split('\n## ', 2);
 const mainChangelog = fs.readFileSync('./docs/changelog.md').toString();
 const [date] = new Date().toISOString().split('T');
-const logWithDate = log.replace('\n', ' (${date})\n');
+const logWithDate = log.replace('\n', ` (${date})\n`);
 const modifiedChangelog = mainChangelog.replace(
   '<!-- do-not-remove -->\n',
   '<!-- do-not-remove -->\n\n## ' + logWithDate
