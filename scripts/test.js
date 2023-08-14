3f7d54bb50b6e79c9993fb50633efcaa4b300855
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2023-08-14 21:28:41 +0300

    chore: test

--- scripts/test.js
+++ scripts/test.js
@@ -8,10 +8,10 @@ const [, logCore] = generatedLogsCore.split('\n## ', 2);
 fs.mkdirSync('./output', { recursive: true });
 fs.writeFileSync(
   './output/release-message.txt',
-  // JSON.stringify(
+  JSON.stringify(
     slackifyMarkdown(
     `:bookmark: New @redocly/cli release ${logCore}\n\n:bookmark: New @redocly/openapi-core release ${logCore}\n\n`
 
-    )
-    // )
+  )
+  ).slice(1, -1)
 );
