747b6eefe5a62cd8f7d63825f37a1b9d4e9bf862
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2023-08-15 10:40:42 +0300

    chore: fix creating release message file (#1228)

--- scripts/post-changeset.js
+++ scripts/post-changeset.js
@@ -1,17 +1,9 @@
 const fs = require('fs');
-const slackifyMarkdown = require('slackify-markdown');
 
 const generatedLogsCli = fs.readFileSync('./packages/cli/CHANGELOG.md').toString();
 const [, logCli] = generatedLogsCli.split('\n## ', 2);
 const generatedLogsCore = fs.readFileSync('./packages/core/CHANGELOG.md').toString();
 const [, logCore] = generatedLogsCore.split('\n## ', 2);
-
-fs.writeFileSync(
-  './output/release-message.txt',
-  slackifyMarkdown(
-    `:bookmark: New @redocly/cli release ${logCli}\n\n:bookmark: New @redocly/openapi-core release ${logCore}\n\n`
-  )
-);
 
 const mainChangelog = fs.readFileSync('./docs/changelog.md').toString();
 const [date] = new Date().toISOString().split('T');
---
+++ scripts/write-release-message.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const slackifyMarkdown = require('slackify-markdown');
+
+const generatedLogsCli = fs.readFileSync('./packages/cli/CHANGELOG.md').toString();
+const [, logCli] = generatedLogsCli.split('\n## ', 2);
+const generatedLogsCore = fs.readFileSync('./packages/core/CHANGELOG.md').toString();
+const [, logCore] = generatedLogsCore.split('\n## ', 2);
+
+fs.mkdirSync('./output', { recursive: true });
+fs.writeFileSync(
+  './output/release-message.json',
+  JSON.stringify({
+    text: slackifyMarkdown(
+      `:bookmark: New @redocly/cli release ${logCli}\n\n:bookmark: New @redocly/openapi-core release ${logCore}\n\n`
+    ),
+  })
+);
