21b5e5322e6a8a1008ea4d5251e898125b130697
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2023-09-08 11:57:42 +0300

    chore: adjust new redoc static file size

--- __tests__/smoke/run-smoke.sh
+++ __tests__/smoke/run-smoke.sh
@@ -17,9 +17,9 @@ $2 redocly-lint
 $2 redocly-bundle
 $2 redocly-build-docs
 # Check for broken styles (related issue: https://github.com/Redocly/redocly-cli/issues/1073)
-if [[ "$(wc -l redoc-static.html)" == "320 redoc-static.html" ]]; then
+if [[ "$(wc -l redoc-static.html)" == "294 redoc-static.html" ]]; then
   echo "Docs built correctly."
 else
-  echo "Docs built incorrectly. Received lines: $(wc -l redoc-static.html) (expected 320 lines in redoc-static.html)."
+  echo "Docs built incorrectly. Received lines: $(wc -l redoc-static.html) (expected 294 lines in redoc-static.html)."
   exit 1
 fi
