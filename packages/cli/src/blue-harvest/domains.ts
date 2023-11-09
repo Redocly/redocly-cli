ce66354cbcb04a5c6ffffce8700f5acbe3c87bbd
Roman Sainchuk <albuman32@gmail.com>
2023-11-09 09:45:46 +0200

    chore: move cli related function out of the core

--- packages/cli/src/blue-harvest/domains.ts
+++ packages/cli/src/blue-harvest/domains.ts
@@ -1,10 +1,10 @@
-import { env } from '../../env';
-
 const DEFAULT_DOMAIN = 'https://app.beta.redocly.com';
 
 export function getDomain(): string {
-  if (env.REDOCLY_DOMAIN) {
-    return env.REDOCLY_DOMAIN;
+  const domain = process.env.REDOCLY_DOMAIN;
+
+  if (domain) {
+    return domain;
   }
 
   return DEFAULT_DOMAIN;
