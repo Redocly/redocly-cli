4868a5b84086eec3904257ab4de85aa9bbdd93bc
Lorna Jane Mitchell <github@lornajane.net>
2024-01-31 17:13:41 +0000

    feat: add markdown output format to stats command (#1395)
    * fix: Remove non-json format elements from JSON output
    
    * feat: Add markdown output format, suitable for use in GitHub Job Summaries
    
    * chore: run prettier, update snapshot for existing tests
    
    * chore: add tests for all the stats output formats
    
    * chore: add museum file, run prettier
    
    * fix: Tidy up some ends spotted by eslint
    
    * fix: Tidy up some ends spotted by eslint
    
    * feat: reinstate debug/timing output for stylish format only
    
    * chore: update snapshots with timing output
    
    * chore: run prettier
    
    * feat: Add markdown table formatting to stats markdown output
    
    * chore: update snapshots for new output format
    
    * docs: Add changeset for markdown format option in stats command

---
+++ .changeset/pink-waves-press.md
@@ -0,0 +1,5 @@
+---
+"@redocly/cli": patch
+---
+
+Added markdown format option to stats command for use with GitHub job summaries.
---
+++ __tests__/stats/stats-json/snapshot.js
@@ -0,0 +1,39 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`E2E stats stats should produce correct JSON output 1`] = `
+{
+  "refs": {
+    "metric": "🚗 References",
+    "total": 39
+  },
+  "externalDocs": {
+    "metric": "📦 External Documents",
+    "total": 0
+  },
+  "schemas": {
+    "metric": "📈 Schemas",
+    "total": 22
+  },
+  "parameters": {
+    "metric": "👉 Parameters",
+    "total": 6
+  },
+  "links": {
+    "metric": "🔗 Links",
+    "total": 0
+  },
+  "pathItems": {
+    "metric": "➡️  Path Items",
+    "total": 5
+  },
+  "operations": {
+    "metric": "👷 Operations",
+    "total": 8
+  },
+  "tags": {
+    "metric": "🔖 Tags",
+    "total": 3
+  }
+}
+
+`;
