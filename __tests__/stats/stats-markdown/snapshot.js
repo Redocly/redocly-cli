c0055decb90166cc983cb2ab74467dc73a493af3
Lorna Mitchell <lorna.mitchell@redocly.com>
2024-01-31 14:05:27 +0000

    chore: update snapshots for new output format

--- __tests__/stats/stats-markdown/snapshot.js
+++ __tests__/stats/stats-markdown/snapshot.js
@@ -1,14 +1,16 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
 exports[`E2E stats stats should produce correct Markdown format 1`] = `
-- 🚗 References: 39
-- 📦 External Documents: 0
-- 📈 Schemas: 22
-- 👉 Parameters: 6
-- 🔗 Links: 0
-- ➡️  Path Items: 5
-- 👷 Operations: 8
-- 🔖 Tags: 3
+| Feature  | Count  |
+| --- | --- |
+| 🚗 References | 39 |
+| 📦 External Documents | 0 |
+| 📈 Schemas | 22 |
+| 👉 Parameters | 6 |
+| 🔗 Links | 0 |
+| ➡️  Path Items | 5 |
+| 👷 Operations | 8 |
+| 🔖 Tags | 3 |
 
 
 `;
