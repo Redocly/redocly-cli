49d081afe3fe0f428a655db1a76cdae130d9bcbe
Mateusz Lis <mateusz.lis@redocly.com>
2024-02-02 09:43:13 +0100

    fix: small refactor

--- packages/core/src/rules/common/no-required-schema-properties-undefined.ts
+++ packages/core/src/rules/common/no-required-schema-properties-undefined.ts
@@ -11,30 +11,34 @@ export const NoRequiredSchemaPropertiesUndefined: Oas3Rule | Oas2Rule = () => {
         schema: Oas3Schema | Oas3_1Schema | Oas2Schema,
         { location, report, resolve }: UserContext
       ) {
+        if (!schema.required) return;
+
         const allProperties = schema.properties ?? {};
 
         // Skip oneOf/anyOf as it's complicated to validate it right now.
         // We need core support for checking contrstrains through those keywords.
         // Right now we only resolve basic allOf keyword nesting.
         if (schema.allOf) {
-          schema.allOf.forEach((nestedSchema: Oas3Schema | Oas3_1Schema | Oas2Schema) => {
+          for (const nestedSchema of schema.allOf as Array<
+            Oas3Schema | Oas3_1Schema | Oas2Schema
+          >) {
             if (isRef(nestedSchema)) {
               const resolved = resolve(nestedSchema).node as Oas3Schema | Oas3_1Schema | Oas2Schema;
               Object.assign(allProperties, resolved.properties);
+            } else {
+              Object.assign(allProperties, nestedSchema.properties);
             }
-
-            Object.assign(allProperties, nestedSchema.properties);
-          });
+          }
         }
 
-        schema.required?.forEach((requiredProperty, i) => {
+        for (const [i, requiredProperty] of schema.required.entries()) {
           if (!allProperties || allProperties[requiredProperty] === undefined) {
             report({
               message: `Required property '${requiredProperty}' is undefined.`,
               location: location.child(['required', i]),
             });
           }
-        });
+        }
       },
     },
   };
