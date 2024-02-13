57c2e917b5030a5e490eff32e02b98dac4bfecdc
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2024-02-13 10:56:18 +0200

    chore: add historical performance benchmarking

---
+++ benchmark/make-test-command.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eo pipefail # Fail on script errors
+
+# Clone the repo with test openapi files:
+git clone https://github.com/Rebilly/api-definitions.git
+
+# Store the command into a text file:
+echo hyperfine -i --warmup 3 $(cat package.json | jq '.dependencies' | jq 'keys' | jq 'map("'\''node node_modules/" + . + "/bin/cli.js lint api-definitions/openapi/openapi.yaml'\''")' | jq 'join(" ")' | xargs) --export-markdown benchmark_check.md --export-json benchmark_check.json > test-command.txt
+
+# Put the command in the test section of the package.json:
+cat package.json | jq ".scripts.test = \"$(cat test-command.txt)\"" > package.json
