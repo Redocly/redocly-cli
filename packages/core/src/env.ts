8d3bb257a12812dcfb0b547004d14c403a5de070
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2024-03-13 15:43:14 +0200

    update to config 0.1.1

--- packages/core/src/env.ts
+++ packages/core/src/env.ts
@@ -1,5 +1,5 @@
 export const isBrowser =
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  typeof window !== 'undefined' || typeof process === 'undefined'; // main and worker thread
+  typeof window !== 'undefined' || typeof process === 'undefined' || process?.platform === 'browser'; // main and worker thread
 export const env = isBrowser ? {} : process.env || {};
