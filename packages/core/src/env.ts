1361d319d149fa6338d164aa2ac5bdfc6c61849c
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2024-03-13 16:52:10 +0200

    remove env.ts changes

--- packages/core/src/env.ts
+++ packages/core/src/env.ts
@@ -1,7 +1,5 @@
 export const isBrowser =
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  typeof window !== 'undefined' ||
-  typeof process === 'undefined' ||
-  (process?.platform as any) === 'browser'; // main and worker thread
+  typeof window !== 'undefined' || typeof process === 'undefined'; // main and worker thread
 export const env = isBrowser ? {} : process.env || {};
