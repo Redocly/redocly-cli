cc9c4b2f38100c6a316a395f712952d3d2f578b8
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2024-04-04 16:24:38 +0300

    chore: update scripts

--- scripts/local-pack.sh
+++ scripts/local-pack.sh
@@ -6,3 +6,6 @@ version=$(cat ./packages/core/package.json | jq '.version' | tr -d '"')
 jq '.dependencies."@redocly/openapi-core" = $packagefile' ./packages/cli/package.json --arg packagefile ./openapi-core.tgz > package.json.tmp && mv package.json.tmp ./packages/cli/package.json &&
 
 cd packages/cli && cli=$(npm pack | tail -n 1) && mv $cli ../../redocly-cli.tgz
+
+# Revert changes to package.json
+cd ../../ && git checkout packages/cli/package.json
