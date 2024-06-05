98a6b944efe813acebec9f19387106a50e0c25ed
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2024-06-05 10:02:39 +0300

    fix: falsy values in no-invalid-media-type-exampls rule

---
+++ .changeset/strong-crews-change.md
@@ -0,0 +1,6 @@
+---
+"@redocly/openapi-core": patch
+"@redocly/cli": patch
+---
+
+Fixed the `no-invalid-media-type-examples` rule which allowed falsy example values to pass for any schema.
--- packages/core/src/rules/oas3/no-invalid-media-type-examples.ts
+++ packages/core/src/rules/oas3/no-invalid-media-type-examples.ts
@@ -12,9 +12,9 @@ export const ValidContentExamples: Oas3Rule = (opts) => {
       leave(mediaType, ctx: UserContext) {
         const { location, resolve } = ctx;
         if (!mediaType.schema) return;
-        if (mediaType.example) {
+        if (typeof mediaType.example !== 'undefined') {
           resolveAndValidateExample(mediaType.example, location.child('example'));
-        } else if (mediaType.examples) {
+        } else if (typeof mediaType.examples !== 'undefined') {
           for (const exampleName of Object.keys(mediaType.examples)) {
             resolveAndValidateExample(
               mediaType.examples[exampleName],
