b2af7789e622015310c73a0665790e254c1030c3
dimaananskyi <dmytro@redocly.com>
2024-07-16 14:31:31 +0300

    feat: export more individual types for arazzo

--- packages/core/src/typings/arazzo.ts
+++ packages/core/src/typings/arazzo.ts
@@ -1,4 +1,44 @@
 import type { FromSchema } from 'json-schema-to-ts';
-import type { arazzoSchema } from '../types/arazzo';
+import type {
+  arazzoSchema,
+  parameter,
+  operationMethod,
+  expectSchema,
+  sourceDescriptionSchema,
+  infoObject,
+  requestBody,
+  replacement,
+  inherit,
+  criteriaObject,
+  step,
+  workflow,
+} from '../types/arazzo';
 
 export type ArazzoDefinition = FromSchema<typeof arazzoSchema>;
+export type OperationMethod = FromSchema<typeof operationMethod>;
+export type ResponseContext = {
+  statusCode: number;
+  body: any;
+  headers: Headers;
+  mimeType: string;
+} & Record<string, any>;
+export type Expect = FromSchema<typeof expectSchema>;
+export type SourceDescription = FromSchema<typeof sourceDescriptionSchema>;
+export type Parameter = FromSchema<typeof parameter>;
+export type InfoObject = FromSchema<typeof infoObject>;
+export type RequestBody = FromSchema<typeof requestBody>;
+export type Replacement = FromSchema<typeof replacement>;
+export type Inherit = FromSchema<typeof inherit>;
+export type CriteriaObject = FromSchema<typeof criteriaObject>;
+export type VerboseLog = {
+  method: OperationMethod;
+  path: string;
+  host: string;
+  body?: any;
+  headerParams?: Record<string, string>;
+  statusCode?: number;
+};
+export type Step = FromSchema<typeof step>;
+export type Workflow = FromSchema<typeof workflow> & {
+  steps: Step[];
+};
