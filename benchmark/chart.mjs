d90182a4f623249435492a554a9a5027da230f41
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2024-08-23 13:50:42 +0300

    update chart table

--- benchmark/chart.mjs
+++ benchmark/chart.mjs
@@ -9,22 +9,20 @@ const arr = json.results.map((r) => [
 const min = arr.reduce(($, [_, mean]) => Math.min($, mean), Infinity);
 const max = arr.reduce(($, [_, mean]) => Math.max($, mean), -Infinity);
 
-const MAX_BAR_LENGTH = 30;
-
 const constructBarForChart = (x) => {
+  const MAX_BAR_LENGTH = 30;
   const length = Math.floor(x * MAX_BAR_LENGTH);
   return '▓' + '▓'.repeat(length) + ' '.repeat(MAX_BAR_LENGTH - length);
 };
 
 const bars = arr.map(
-  ([cliVersion, mean]) => `│ ${cliVersion} │ ${constructBarForChart((mean - min) / (max - min))} │`
+  ([cliVersion, mean]) => `| ${cliVersion} | ${constructBarForChart((mean - min) / (max - min))} |`
 );
 
 const output = [
-  '           TIME CHART',
-  '┌' + '─'.repeat(10) + '┬' + '─'.repeat(MAX_BAR_LENGTH + 3) + '┐',
+  '| CLI Version | Performance benchmark (test duration) |',
+  '|---|---|',
   ...bars,
-  '└' + '─'.repeat(10) + '┴' + '─'.repeat(MAX_BAR_LENGTH + 3) + '┘\n',
 ].join('\n');
 
 process.stdout.write(output);
