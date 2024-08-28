5ccf79f7af1843b62e7c00d7322fcf9dbcf1c716
dimaananskyi <dmytro@redocly.com>
2024-08-28 11:59:36 +0300

    feat: update enum rule text

--- packages/core/src/rules/spot/arazzo-version-enum.ts
+++ packages/core/src/rules/spot/arazzo-version-enum.ts
@@ -4,12 +4,13 @@ import type { ArazzoRule } from '../../visitors';
 import type { UserContext } from '../../walk';
 
 export const ArazzoVersionEnum: ArazzoRule = () => {
+  const ALOOWED_ARAZZO_VERSIONS_STRING = ARAZZO_VERSIONS_SUPPORTED_BY_SPOT.join(', ');
   return {
     Root: {
       enter(root, { report, location }: UserContext) {
         if (!ARAZZO_VERSIONS_SUPPORTED_BY_SPOT.includes(root.arazzo)) {
           report({
-            message: 'Only `1.0.0` Arazzo version is supported by Spot.',
+            message: `Only ${ALOOWED_ARAZZO_VERSIONS_STRING} Arazzo version ${ARAZZO_VERSIONS_SUPPORTED_BY_SPOT.length > 1 ? 'are' : 'is'} supported by Spot.`,
             location: location.child('arazzo'),
           });
         }
