6d2a14194c0ed1a44febf179f6688c7e673be317
Luke Hagar <lukeslakemail@gmail.com>
2024-10-03 01:49:20 +0000

    adjusted replace value

--- packages/cli/src/utils/miscellaneous.ts
+++ packages/cli/src/utils/miscellaneous.ts
@@ -131,7 +131,7 @@ export function printExecutionTime(commandName: string, startedAt: number, api: 
 
 export function pathToFilename(path: string, pathSeparator: string) {
   if (path === '/') {
-    return 'root';
+    return '~root';
   }
   return path
     .replace(/~1/g, '/')
