4517b8e658c458597a59b45f607bcdd002a3c283
Vova Rutskyi <rutskyi.v@gmail.com>
2024-10-04 13:05:28 +0300

    chore: split plugins smoke tests into separate workflow

--- scripts/prepare-smoke-plugins.sh
+++ scripts/prepare-smoke-plugins.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# For Workflows (Webpack)
-npm run webpack-bundle 
-
 # For npm (Mutates packages/cli/package.json)
 npm run pack:prepare
 
-cp ./redocly-cli.tgz ./openapi-core.tgz ./dist/bundle.js ./__tests__/smoke/
+cp ./redocly-cli.tgz ./openapi-core.tgz ./__tests__/smoke-plugins
 
 echo "Current directory:"
 pwd
@@ -15,5 +12,5 @@ echo "Current directory content:"
 ls -a
 echo
 echo "Target directory content:"
-ls -a __tests__/smoke/
+ls -a __tests__/smoke-plugins/
 echo
