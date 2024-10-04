fe3e9980f3e2010e37d6366dfef7c20df3133c57
Vova Rutskyi <rutskyi.v@gmail.com>
2024-10-04 13:10:34 +0300

    chore: undo fix to verify the smoke tests will fail

--- packages/core/src/config/config-resolvers.ts
+++ packages/core/src/config/config-resolvers.ts
@@ -139,7 +139,8 @@ export async function resolvePlugins(
         const absolutePluginPath = existsSync(maybeAbsolutePluginPath)
           ? maybeAbsolutePluginPath
           : // For plugins imported from packages specifically
-            require.resolve(plugin, { paths: [configDir] });
+            // require.resolve(plugin, { paths: [configDir] });
+            require.resolve(plugin);
 
         if (!pluginsCache.has(absolutePluginPath)) {
           let requiredPlugin: ImportedPlugin | undefined;
