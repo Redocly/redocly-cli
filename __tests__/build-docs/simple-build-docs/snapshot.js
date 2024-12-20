72ab792191c2e28ecf27b5060b48c7ca6f678c25
Philippe Vignau <philippe@pvg.sh>
2024-12-20 18:41:05 +0100

    test: update snapshots for build-docs command fix

--- __tests__/build-docs/simple-build-docs/snapshot.js
+++ __tests__/build-docs/simple-build-docs/snapshot.js
@@ -1,10 +1,10 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
 exports[`E2E build-docs simple build-docs 1`] = `
+Found undefined and using theme.openapi options
+Prerendering docs
 
 🎉 bundled successfully in: redoc-static.html (324 KiB) [⏱ <test>ms].
 
-Found undefined and using theme.openapi options
-Prerendering docs
 
 `;
