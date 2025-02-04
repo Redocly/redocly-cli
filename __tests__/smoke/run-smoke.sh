668697a56775156b0dd8cb3ec503c60ec26cbb71
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2025-02-04 17:28:11 +0200

    chore: add missing smoke and e2e tests

--- __tests__/smoke/run-smoke.sh
+++ __tests__/smoke/run-smoke.sh
@@ -17,9 +17,9 @@ $2 redocly-lint
 $2 redocly-bundle
 $2 redocly-build-docs
 # Check for broken styles (related issue: https://github.com/Redocly/redocly-cli/issues/1073)
-if [[ "$(wc -l redoc-static.html)" == "294 redoc-static.html" ]]; then
+if [[ "$(wc -l redoc-static.html)" == "300 redoc-static.html" ]]; then
   echo "Docs built correctly."
 else
-  echo "Docs built incorrectly. Received lines: $(wc -l redoc-static.html) (expected 294 lines in redoc-static.html)."
+  echo "Docs built incorrectly. Received lines: $(wc -l redoc-static.html) (expected 300 lines in redoc-static.html)."
   exit 1
 fi
