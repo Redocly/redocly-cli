3154592e9f683e4b8c7e63e2d922844a42861f43
DmitryAnansky <dmytro@redocly.com>
2025-02-10 16:28:00 +0200

    chore: revert snapshots

--- __tests__/build-docs/build-docs-with-config-option/snapshot.js
+++ __tests__/build-docs/build-docs-with-config-option/snapshot.js
@@ -4,7 +4,7 @@ exports[`E2E build-docs build docs with config option 1`] = `
 Found nested/redocly.yaml and using theme.openapi options
 Prerendering docs
 
-🎉 bundled successfully in: nested/redoc-static.html (33 KiB) [⏱ <test>ms].
+🎉 bundled successfully in: nested/redoc-static.html (36 KiB) [⏱ <test>ms].
 
 
 `;
