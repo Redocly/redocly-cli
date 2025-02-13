95eb738fb05e75a5735bae6eaccc4897d475f065
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2025-02-13 19:33:17 +0200

    improve the build-docs smoke test check

--- __tests__/smoke/run-smoke.sh
+++ __tests__/smoke/run-smoke.sh
@@ -20,12 +20,8 @@ $2 redocly-split
 
 # Check for broken styles when building docs (related issue: https://github.com/Redocly/redocly-cli/issues/1073)
 echo "Checking docs for issues..."
-if [[ "$(wc -l redoc-static.html)" == "324 redoc-static.html" ]]; then
-  echo "✅ Docs built correctly."
-else
-  echo "❌ Docs built incorrectly. Received lines: $(wc -l redoc-static.html) (expected 324 lines in redoc-static.html)."
-  exit 1
-fi
+diff pre-built/redoc.html redoc-static.html 
+echo "✅ Docs built correctly."
 
 # Check for broken $refs (or other issues) in the split files, especially on Windows (it will fail on a difference)
 echo "Checking split files for issues..."
