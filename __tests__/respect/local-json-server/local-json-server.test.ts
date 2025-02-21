f92ec301242ed1686e0ca446ede7c87c51285e63
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2025-02-21 17:55:24 +0200

    chore: use json server test (#1937)

--- __tests__/respect/local-json-server/local-json-server.test.ts
+++ __tests__/respect/local-json-server/local-json-server.test.ts
@@ -1,16 +1,26 @@
-// import { getParams, getCommandOutput } from '../utils';
-// import { join } from 'path';
+import { spawn } from 'child_process';
+import { getParams, getCommandOutput } from '../utils';
+import { join } from 'path';
 
-// test('local-json-server test case', () => {
-//   const indexEntryPoint = join(process.cwd(), 'packages/cli/lib/index.js');
-//   const fixturesPath = join(__dirname, 'local-json-server.yaml');
-//   const args = getParams(indexEntryPoint, ['respect', fixturesPath]);
+describe('local-json-server', () => {
+  let serverProcess: any;
 
-//   const result = getCommandOutput(args);
-//   expect(result).toMatchSnapshot();
-// });
+  beforeAll(async () => {
+    // Start json-server
+    serverProcess = spawn('npm', ['run', 'json-server'], { detached: true });
+  });
 
-test('local-json-server test case', () => {
-  //TODO: Remove.Disable this test until json-server is added to the e2e flow.
-  expect(true).toBe(true);
+  afterAll(() => {
+    // Kill the process group to ensure child processes are cleaned up
+    process.kill(-serverProcess.pid);
+  });
+
+  test('local-json-server test case', () => {
+    const indexEntryPoint = join(process.cwd(), 'packages/cli/lib/index.js');
+    const fixturesPath = join(__dirname, 'local-json-server.yaml');
+    const args = getParams(indexEntryPoint, ['respect', fixturesPath]);
+
+    const result = getCommandOutput(args);
+    expect(result).toMatchSnapshot();
+  });
 });
