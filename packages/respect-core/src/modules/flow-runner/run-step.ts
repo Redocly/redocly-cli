4756c26f5e8e7a9d013b03699ddf235404b9fd62
Roman Hotsiy <gotsijroman@gmail.com>
2025-02-24 20:44:48 +0800

    chore: rename env var

--- packages/respect-core/src/modules/flow-runner/run-step.ts
+++ packages/respect-core/src/modules/flow-runner/run-step.ts
@@ -32,7 +32,7 @@ import type { ParameterWithoutIn } from '../config-parser';
 
 const logger = DefaultLogger.getInstance();
 
-const MAX_STEPS = parseInt(process.env.RESPECT_MAX_STEPS || '2000', 10);
+const MAX_STEPS = parseInt(process.env.REDOCLY_CLI_RESPECT_MAX_STEPS || '2000', 10);
 let stepsRun = 0;
 
 export async function runStep({
