ba27a3cf12f9eb1a2e3ded9daeca4eb02aec26d8
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2025-03-05 16:31:47 +0200

    feat: migrate to ESM (drop webpack + deploy on AWS s3)

--- __tests__/smoke/prepare-smoke.sh
+++ __tests__/smoke/prepare-smoke.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# For Workflows (Webpack)
-npm run webpack-bundle 
-
 # For npm (Mutates packages/cli/package.json)
 npm run pack:prepare
 
-cp ./redocly-cli.tgz ./openapi-core.tgz ./respect-core.tgz ./dist/bundle.js resources/pets.yaml resources/museum.yaml resources/museum-tickets.arazzo.yaml ./__tests__/smoke/
+cp ./redocly-cli.tgz ./openapi-core.tgz ./respect-core.tgz resources/pets.yaml resources/museum.yaml resources/museum-tickets.arazzo.yaml ./__tests__/smoke/
 
 echo "Current directory:"
 pwd
