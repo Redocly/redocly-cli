0405186c30be3de8fb3687b882589ce0626dfece
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2025-03-10 14:39:20 +0200

    remove threshold

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 const { readFileSync } = require('fs');
 const path = require('path');
 
-const migratedSuites = JSON.parse(readFileSync(path.resolve(__dirname, 'migrated-suites.json'), 'utf-8'));
+const migratedSuites = JSON.parse(
+  readFileSync(path.resolve(__dirname, 'migrated-suites.json'), 'utf-8')
+);
 
 module.exports = {
   clearMocks: true,
@@ -16,26 +18,7 @@ module.exports = {
     '!packages/cli/src/index.ts',
     '!packages/cli/src/utils/assert-node-version.ts',
   ],
-  coverageThreshold: {
-    'packages/core/': {
-      statements: 84,
-      branches: 72,
-      functions: 76,
-      lines: 84,
-    },
-    'packages/cli/': {
-      statements: 63,
-      branches: 51,
-      functions: 62,
-      lines: 63,
-    },
-    'packages/respect-core/': {
-      statements: 80,
-      branches: 72,
-      functions: 80,
-      lines: 80,
-    },
-  },
+
   testMatch: ['**/__tests__/**/*.test.ts', '**/*.test.ts'],
   globals: {
     'ts-jest': {
