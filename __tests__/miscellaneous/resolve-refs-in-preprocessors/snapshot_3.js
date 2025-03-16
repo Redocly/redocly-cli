db47c12a27e0d1df500db166397f62d646789459
Andrii Maryskevych <andrii.maryskevych@redocly.com>
2025-03-16 08:42:32 +0200

    RESOLVE_MULTI_EXPORT_IN_SNAPSHOT:: miscellaneous/resolve-refs-in-preprocessors/snapshot.js

---
+++ __tests__/miscellaneous/resolve-refs-in-preprocessors/snapshot_3.js
@@ -0,0 +1,20 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`E2E miscellaneous stat should print the correct summary with $refs in preprocessors 1`] = `
+
+Document: openapi.yaml stats:
+
+🚗 References: 3 
+📦 External Documents: 0 
+📈 Schemas: 1 
+👉 Parameters: 0 
+🔗 Links: 0 
+🔀 Path Items: 2 
+🎣 Webhooks: 0 
+👷 Operations: 2 
+🔖 Tags: 0 
+
+openapi.yaml: stats processed in <test>ms
+
+
+`;
