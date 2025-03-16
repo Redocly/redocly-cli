4618bd12e06f4576ba62650c955d8f18af0acc9d
Andrii Maryskevych <andrii.maryskevych@redocly.com>
2025-03-16 09:15:23 +0200

    MIGRATION:: build-docs

--- __tests__/build-docs/simple-build-docs/snapshot.js
+++ __tests__/build-docs/simple-build-docs/snapshot.js
@@ -1,10 +1,5 @@
-// Jest Snapshot v1, https://goo.gl/fbAQLP
-
-exports[`E2E build-docs simple build-docs 1`] = `
 Found undefined and using theme.openapi options
 Prerendering docs
 
 🎉 bundled successfully in: redoc-static.html (330 KiB) [⏱ <test>ms].
 
-
-`;
