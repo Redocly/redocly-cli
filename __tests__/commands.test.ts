05bd7c0e4b2bd2b7b849336b0c3b44e275c276c8
Andrii Maryskevych <andrii.maryskevych@redocly.com>
2025-03-16 09:03:08 +0200

    RESOLVE_MULTI_EXPORT_IN_SNAPSHOT:: bundle/bundle-description-dereferenced/snapshot.js

--- __tests__/commands.test.ts
+++ __tests__/commands.test.ts
@@ -545,7 +545,9 @@ describe('E2E', () => {
       ]);
 
       const result = getCommandOutput(args, folderPath);
-      (<any>expect(cleanupOutput(result))).toMatchSpecificSnapshot(join(folderPath, 'snapshot.js'));
+      (<any>expect(cleanupOutput(result))).toMatchSpecificSnapshot(
+        join(folderPath, 'snapshot_2.js')
+      );
     });
 
     it('discriminator mapping should be replaced with correct references to components', () => {
