3fd360275e9b2f251d8187381f2b3b4ed3e3a956
Andrii Maryskevych <andrii.maryskevych@redocly.com>
2025-03-16 13:53:23 +0200

    MIGRATION:: bundle with option in config: remove-unused-components

--- __tests__/bundle/bundle-remove-unused-components-from-config/oas3-without-option/without-remove-unused-components-snapshot.js
+++ __tests__/bundle/bundle-remove-unused-components-from-config/oas3-without-option/without-remove-unused-components-snapshot.js
@@ -1,6 +1,3 @@
-// Jest Snapshot v1, https://goo.gl/fbAQLP
-
-exports[`E2E bundle with option in config: remove-unused-components oas3-without-option: shouldn't remove unused components 1`] = `
 openapi: 3.1.0
 paths:
   /store/order:
@@ -55,5 +52,3 @@ components:
 
 bundling openapi.yaml...
 📦 Created a bundle for openapi.yaml at stdout <test>ms.
-
-`;
