c4b37b08d15fbf7df21a236d7a171c295bb49ceb
Andrii Maryskevych <andrii.maryskevych@redocly.com>
2025-03-17 08:22:50 +0200

    MIGRATION:: join

--- __tests__/join/multiple-tags-in-same-files/snapshot.js
+++ __tests__/join/multiple-tags-in-same-files/snapshot.js
@@ -1,6 +1,3 @@
-// Jest Snapshot v1, https://goo.gl/fbAQLP
-
-exports[`E2E join without options test: multiple-tags-in-same-files 1`] = `
 
 openapi: 3.0.0
 info:
@@ -82,5 +79,3 @@ x-tagGroups:
 
 openapi.yaml: join processed in <test>ms
 
-
-`;
