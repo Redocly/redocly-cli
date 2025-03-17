fc6c4e516a16933385dd82eda9c6e300dd4ea4c7
Andrii Maryskevych <andrii.maryskevych@redocly.com>
2025-03-17 14:55:17 +0200

    MIGRATION:: lint-config

--- __tests__/lint-config/invalid-config--lint-config-off/snapshot.js
+++ __tests__/lint-config/invalid-config--lint-config-off/snapshot.js
@@ -1,6 +1,3 @@
-// Jest Snapshot v1, https://goo.gl/fbAQLP
-
-exports[`E2E lint-config test with option: { dirName: 'invalid-config--lint-config-off', option: 'off' } 1`] = `
 
 validating ../__fixtures__/valid-openapi.yaml...
 ../__fixtures__/valid-openapi.yaml: validated in <test>ms
@@ -9,5 +6,3 @@ Woohoo! Your API description is valid. 🎉
 
 [WARNING] Unused rules found in .redocly.yaml: context.
 Check the spelling and verify the added plugin prefix.
-
-`;
