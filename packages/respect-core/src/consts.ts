37ea63cfeba02babce195dcabce9186b68bcb0f3
DmitryAnansky <dmytro@redocly.com>
2025-03-18 18:47:19 +0200

    chore: restore prev timeout

--- packages/respect-core/src/consts.ts
+++ packages/respect-core/src/consts.ts
@@ -1,3 +1,3 @@
 export const DEFAULT_RESPECT_TIMEOUT = 3600000; // 1 hour in milliseconds
-export const DEFAULT_RESPECT_MAX_FETCH_TIMEOUT = 60000; // 20 seconds in milliseconds
+export const DEFAULT_RESPECT_MAX_FETCH_TIMEOUT = 20000; // 20 seconds in milliseconds
 export const DEFAULT_RESPECT_MAX_STEPS = 2000;
