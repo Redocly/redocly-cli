3ffd31f74ca42fd9a9a140fb4b7a7c7b5f61509c
Roman Hotsiy <gotsijroman@gmail.com>
2025-04-09 16:09:46 +0800

    chore: clean comments

--- benchmark/chart.mjs
+++ benchmark/chart.mjs
@@ -2,20 +2,19 @@ import fs from 'node:fs';
 
 const content = fs.readFileSync('benchmark_check.json', 'utf8');
 const json = JSON.parse(content);
-// Extract command, mean, and stddev
 const arr = json.results.map((r) => [
   r.command.replace(/^node node_modules\/([^/]+)\/.*/, (_, cliVersion) => cliVersion),
   r.mean,
-  r.stddev, // Assuming the field name is stddev
+  r.stddev,
 ]);
 const minMean = Math.min(...arr.map(([_, mean]) => mean));
 
 const constructBarForChart = (mean, min) => {
-  if (min <= 0) return 'N/A'; // Avoid division by zero or nonsensical results
+  if (min <= 0) return 'N/A';
   const slownessRatio = mean / min;
   const slownessFactor = slownessRatio - 1;
   const maxBarLength = 30;
-  const visualFactor = Math.min(1, slownessFactor); // Cap bar growth at 100% slower
+  const visualFactor = Math.min(1, slownessFactor);
   const length = Math.floor(visualFactor * maxBarLength);
   return '▓' + '▓'.repeat(length);
 };
@@ -30,9 +29,7 @@ const output = [
     const relativeSpeedFactor = (mean / minMean).toFixed(2);
     const factorSuffix = mean === minMean ? 'x (Fastest)' : 'x';
 
-    // Combine mean and stddev
     const timeWithStddev = `${meanFormatted}s ± ${stddevFormatted}s`;
-    // Combine bar and relative speed factor
     const performanceDisplay = `${bar} ${relativeSpeedFactor}${factorSuffix}`;
 
     return `| ${cliVersion} | ${timeWithStddev} | ${performanceDisplay} |`;
