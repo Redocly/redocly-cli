6020c21e7019155f2a02a9232218656124436ebd
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2025-04-16 10:27:58 +0300

    chore: update benchmark setup

--- benchmark/make-test-command.sh
+++ benchmark/make-test-command.sh
@@ -6,7 +6,7 @@ set -eo pipefail # Fail on script errors
 git clone https://github.com/Rebilly/api-definitions.git
 
 # Store the command into a text file:
-echo REDOCLY_SUPPRESS_UPDATE_NOTICE=true hyperfine -i --warmup 3 $(cat package.json | jq '.dependencies' | jq 'keys' | jq 'map("'\''node node_modules/" + . + "/bin/cli.js lint core@public --config=api-definitions/redocly.yaml'\''")' | jq 'join(" ")' | xargs) --export-markdown benchmark_check.md --export-json benchmark_check.json > test-command.txt
+echo REDOCLY_SUPPRESS_UPDATE_NOTICE=true hyperfine --warmup 3 $(cat package.json | jq '.dependencies' | jq 'keys' | jq 'map("'\''node node_modules/" + . + "/bin/cli.js lint --config=api-definitions/redocly.yaml --generate-ignore-file'\''")' | jq 'join(" ")' | xargs) --export-markdown benchmark_check.md --export-json benchmark_check.json > test-command.txt
 
 # Put the command in the test section of the package.json:
 cat package.json | jq ".scripts.test = \"$(cat test-command.txt)\"" > package.json
