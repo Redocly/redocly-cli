826bdf66f5bb0658e979d59a66f1c0fd2fe17160
Yaroslav Shuhailo <yaroslav@redocly.com>
2025-05-13 17:22:58 +0300

    feat(respect): add validation for x-security parameters and update resolution logic

--- packages/respect-core/src/modules/flow-runner/resolve-x-security-parameters.ts
+++ packages/respect-core/src/modules/flow-runner/resolve-x-security-parameters.ts
@@ -1,10 +1,12 @@
 import { evaluateRuntimeExpressionPayload } from '../runtime-expressions/index.js';
 import { getSecurityParameters } from '../config-parser/get-security-parameters.js';
+import { validateXSecurityParameters } from './validate-x-security-parameters.js';
 
 import type { ExtendedSecurity } from 'core/src/typings/arazzo.js';
 import type { ParameterWithIn } from '../config-parser/index.js';
 import type { Step, RuntimeExpressionContext } from '../../types.js';
 import type { OperationDetails } from '../description-parser/get-operation-from-description.js';
+import type { Oas3SecurityScheme } from 'core/src/typings/openapi.js';
 
 export function resolveXSecurityParameters(
   ctx: RuntimeExpressionContext,
@@ -20,7 +22,7 @@ export function resolveXSecurityParameters(
   return xSecurity.map((security) => {
     const scheme =
       'schemeName' in security
-        ? operation?.securitySchemes?.[security.schemeName]
+        ? (operation?.securitySchemes?.[security.schemeName] as Oas3SecurityScheme)
         : security.scheme;
 
     const values = Object.fromEntries(
@@ -30,9 +32,11 @@ export function resolveXSecurityParameters(
       ])
     );
 
-    return getSecurityParameters({
+    const resolvedSecurity = validateXSecurityParameters({
       scheme,
       values,
-    } as any);
+    });
+
+    return getSecurityParameters(resolvedSecurity);
   });
 }
---
+++ packages/respect-core/src/modules/flow-runner/validate-x-security-parameters.ts
@@ -0,0 +1,32 @@
+import type { Oas3SecurityScheme } from 'core/src/typings/openapi';
+import type { ResolvedSecurity } from 'core/src/typings/arazzo';
+
+const REQUIRED_VALUES_MAP = {
+  apiKey: ['value'],
+  basic: ['username', 'password'],
+  digest: ['username', 'password'],
+  bearer: ['token'],
+  oauth2: ['accessToken'],
+} as const;
+
+export function validateXSecurityParameters({
+  scheme,
+  values,
+}: {
+  scheme: Oas3SecurityScheme;
+  values: Record<string, string>;
+}): ResolvedSecurity {
+  const schemeType = scheme.type === 'http' ? scheme.scheme : scheme.type;
+
+  const requiredKeys = REQUIRED_VALUES_MAP[schemeType as keyof typeof REQUIRED_VALUES_MAP];
+
+  if (requiredKeys) {
+    for (const key of requiredKeys) {
+      if (!values?.[key]) {
+        throw new Error(`Missing required value \`${key}\` for ${schemeType} security scheme`);
+      }
+    }
+  }
+
+  return { scheme, values } as ResolvedSecurity;
+}
