7abe049f75c85d0fedce173dc61a95165f4495c7
Yaroslav Shuhailo <yaroslav@redocly.com>
2025-05-14 19:20:37 +0300

    feat(respect): validate x security (#2087)

--- packages/respect-core/src/modules/flow-runner/resolve-x-security-parameters.ts
+++ packages/respect-core/src/modules/flow-runner/resolve-x-security-parameters.ts
@@ -1,62 +1,39 @@
 import { evaluateRuntimeExpressionPayload } from '../runtime-expressions/index.js';
+import { getSecurityParameters } from '../config-parser/get-security-parameters.js';
+import { resolveXSecurity } from './validate-x-security-parameters.js';
 
+import type { ExtendedSecurity } from 'core/src/typings/arazzo.js';
 import type { ParameterWithIn } from '../config-parser/index.js';
 import type { Step, RuntimeExpressionContext } from '../../types.js';
 import type { OperationDetails } from '../description-parser/get-operation-from-description.js';
+import type { Oas3SecurityScheme } from 'core/src/typings/openapi.js';
 
 export function resolveXSecurityParameters(
   ctx: RuntimeExpressionContext,
   step: Step,
-  operation: (OperationDetails & Record<string, any>) | undefined
-) {
-  const { 'x-security': xSecurity } = step;
-  const xSecurityParameters: ParameterWithIn[] = [];
+  operation?: OperationDetails & { securitySchemes: Record<string, Oas3SecurityScheme> }
+): ParameterWithIn[] {
+  const xSecurity = step['x-security'] as ExtendedSecurity[] | undefined;
 
   if (!xSecurity) {
-    return xSecurityParameters;
+    return [];
   }
 
-  for (const securityScheme of xSecurity) {
-    const { schemeName, scheme, values } = securityScheme;
+  return xSecurity.map((security) => {
+    const scheme =
+      'schemeName' in security
+        ? (operation?.securitySchemes?.[security.schemeName] as Oas3SecurityScheme)
+        : security.scheme;
 
-    const resolvedValues = Object.entries(values || {}).reduce<Record<string, any>>(
-      (acc, [key, value]) => ({
-        ...acc,
-        [key]: evaluateRuntimeExpressionPayload({ payload: value, context: ctx }),
-      }),
-      {}
+    const values = Object.fromEntries(
+      Object.entries(security?.values ?? {}).map(([key, value]) => [
+        key,
+        evaluateRuntimeExpressionPayload({ payload: value, context: ctx }),
+      ])
     );
 
-    let resolvedSchema = null;
-    if (schemeName) {
-      resolvedSchema = operation?.securitySchemes[schemeName] || null;
-    } else {
-      resolvedSchema = scheme;
-    }
+    const resolvedSecurity = resolveXSecurity({ scheme, values });
 
-    // TODO: replace with the Auth algorithm functions
-    if (resolvedSchema?.type === 'apiKey' && resolvedValues?.value) {
-      xSecurityParameters.push({
-        name: resolvedSchema.name,
-        in: resolvedSchema.in,
-        value: resolvedValues.value as string | number | boolean,
-      });
-    } else if (resolvedSchema?.type === 'http' && resolvedSchema?.scheme === 'basic') {
-      const { username, password } = resolvedValues;
-      xSecurityParameters.push({
-        name: 'Authorization',
-        in: 'header',
-        value: `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`,
-      });
-    } else if (resolvedSchema?.type === 'http' && resolvedSchema?.scheme === 'bearer') {
-      const { token } = resolvedValues;
-      xSecurityParameters.push({
-        name: 'Authorization',
-        in: 'header',
-        value: `Bearer ${token}`,
-      });
-    }
-  }
-
-  return xSecurityParameters;
+    return getSecurityParameters(resolvedSecurity);
+  });
 }
