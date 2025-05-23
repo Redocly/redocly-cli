d0470ccf2d6d094f001e82e8446f250291322ca5
DmitryAnansky <dmytro@redocly.com>
2025-05-23 18:43:56 +0300

    fix: multiple secrets replacement

--- packages/respect-core/src/modules/cli-output/mask-secrets.ts
+++ packages/respect-core/src/modules/cli-output/mask-secrets.ts
@@ -16,12 +16,13 @@ export function maskSecrets<T extends { [x: string]: any } | string>(
 
   const masked = JSON.parse(JSON.stringify(target));
   const maskIfContainsSecret = (value: string): string => {
+    let maskedValue = value;
     for (const secret of secretValues) {
-      if (value.includes(secret)) {
-        return maskValue(value, secret);
+      if (maskedValue.includes(secret)) {
+        maskedValue = maskValue(maskedValue, secret);
       }
     }
-    return value;
+    return maskedValue;
   };
 
   const maskRecursive = (current: any) => {
