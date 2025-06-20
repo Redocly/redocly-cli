fad1996d3153d3125f6ff309b7b65653e6ee35e1
Roman Hotsiy <gotsijroman@gmail.com>
2025-06-20 19:47:25 +0800

    fix: fix no-undefined-server-variable crashing on null in server list

---
+++ packages/core/src/rules/oas3/__tests__/no-undefined-server-variables.test.ts
@@ -0,0 +1,26 @@
+import { outdent } from 'outdent';
+import { lintDocument } from '../../../lint.js';
+import { parseYamlToDocument, replaceSourceWithRef } from '../../../../__tests__/utils.js';
+import { BaseResolver } from '../../../resolve.js';
+import { createConfig } from '../../../config/index.js';
+
+describe('Oas3 no-undefined-server-variable', () => {
+  it('should not crash on null server', async () => {
+    const document = parseYamlToDocument(
+      outdent`
+          openapi: 3.0.0
+          servers:
+            - null
+        `,
+      'foobar.yaml'
+    );
+
+    const results = await lintDocument({
+      externalRefResolver: new BaseResolver(),
+      document,
+      config: await createConfig({ rules: { 'no-undefined-server-variable': 'error' } }),
+    });
+
+    expect(replaceSourceWithRef(results)).toHaveLength(0);
+  });
+});
