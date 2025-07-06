590c07d08e825784bcddeafd4cded3945545a53b
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-07-06 11:49:01 +0300

    fix: remove root tags check and add proper location without child

--- packages/core/src/rules/common/no-duplicated-tag-names.ts
+++ packages/core/src/rules/common/no-duplicated-tag-names.ts
@@ -1,7 +1,5 @@
 import { getTagName } from '../utils.js';
 
-import type { Oas3Definition, Oas3_1Definition } from '../../typings/openapi.js';
-import type { Oas2Definition } from '../../typings/swagger.js';
 import type { Async2Rule, Async3Rule, Oas2Rule, Oas3Rule } from '../../visitors.js';
 import type { UserContext } from '../../walk.js';
 
@@ -12,20 +10,13 @@ export const NoDuplicatedTagNames: Oas3Rule | Oas2Rule | Async2Rule | Async3Rule
 
   return {
     Tag: {
-      leave(
-        tag: { name: string },
-        ctx: UserContext,
-        root: Oas2Definition | Oas3Definition | Oas3_1Definition
-      ) {
-        if (!root.tags) return;
-
+      leave(tag: { name: string }, ctx: UserContext) {
         const tagName = getTagName(tag, ignoreCase);
-        const index = root.tags.findIndex((t) => t === tag);
 
         if (tagNames.has(tagName)) {
           ctx.report({
             message: `Duplicate tag name found: '${tag.name}'`,
-            location: ctx.location.child(['tags', index]),
+            location: ctx.location,
           });
         } else {
           tagNames.add(tagName);
