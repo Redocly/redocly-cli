f51a614ad7a21483750cf814dc07211a79f6d75e
DmitryAnansky <dmytro@redocly.com>
2025-07-07 13:23:52 +0300

    feat: move display-files-summary-table to cli level

--- packages/respect-core/src/modules/cli-output/index.ts
+++ packages/respect-core/src/modules/cli-output/index.ts
@@ -2,6 +2,5 @@ export * from './display-summary.js';
 export * from './display-errors.js';
 export * from './display-checks.js';
 export * from './calculate-tests-passed.js';
-export * from './display-files-summary-table.js';
 export * from './verbose-logs.js';
 export * from './mask-secrets.js';
