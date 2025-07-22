07004e402d90739f3a5ea90c6ac162756f26957c
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2025-07-22 15:34:32 +0300

    chore: fix performance benchmark by adding the installation step (#2205)

--- benchmark/make-test-command.sh
+++ benchmark/make-test-command.sh
@@ -4,6 +4,7 @@ set -eo pipefail # Fail on script errors
 
 # Clone the repo with test openapi files:
 git clone https://github.com/Rebilly/api-definitions.git
+cd api-definitions && npm install && cd ..
 
 # Store the command into a text file:
 echo REDOCLY_SUPPRESS_UPDATE_NOTICE=true hyperfine --warmup 3 $(cat package.json | jq '.dependencies' | jq 'keys' | jq 'map("'\''node node_modules/" + . + "/bin/cli.js lint --config=api-definitions/redocly.yaml --generate-ignore-file'\''")' | jq 'join(" ")' | xargs) --export-markdown benchmark_check.md --export-json benchmark_check.json > test-command.txt
