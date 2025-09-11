67c292429cfbd0311fd2693869536b9915785373
DmitryAnansky <dmytro@redocly.com>
2025-09-11 16:30:48 +0300

    chore: export generated abnfRuntimeExpressionParser from respect-core

--- packages/respect-core/src/modules/runtime-expressions/index.ts
+++ packages/respect-core/src/modules/runtime-expressions/index.ts
@@ -1,1 +1,9 @@
 export * from './evaluate.js';
+
+// Import CommonJS module in ESM
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const { parse } = require('./abnf-parser.cjs');
+
+export const abnfRuntimeExpressionParser = { parse };
