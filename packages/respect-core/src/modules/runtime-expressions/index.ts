4057f10fb65f729dfc4e470af3b7cd1208fdb82c
Dmytro Anansky <dmytro@redocly.com>
2025-09-12 14:47:32 +0300

    chore: export generated parseRuntimeExpression from respect-core (#2294)

--- packages/respect-core/src/modules/runtime-expressions/index.ts
+++ packages/respect-core/src/modules/runtime-expressions/index.ts
@@ -1,1 +1,5 @@
+// @ts-expect-error - Generated PEG.js parser has no TypeScript definitions
+import { parse } from './abnf-parser.js';
+
 export * from './evaluate.js';
+export const parseRuntimeExpression = parse;
--- packages/respect-core/src/modules/runtime-expressions/lint.ts
+++ packages/respect-core/src/modules/runtime-expressions/lint.ts
@@ -1,10 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { default as parser } from './abnf-parser.cjs';
+import { parseRuntimeExpression } from './index.js';
 
 export function lintExpression(expression: string) {
   try {
-    return parser.parse(expression);
+    return parseRuntimeExpression(expression);
   } catch (_error) {
     throw new Error(`Runtime expression is not valid: ${expression}`);
   }
