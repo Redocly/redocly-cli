e75718a4183fe332297bfa9ded56ac66a626005b
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-10-02 13:22:35 +0300

    feat: improve telemetry insights for spec version detection (#2333)

--- packages/cli/src/wrapper.ts
+++ packages/cli/src/wrapper.ts
@@ -28,13 +28,17 @@ export function commandWrapper<T extends CommandArgv>(
   return async (argv: Arguments<T>) => {
     let code: ExitCode = 2;
     let telemetry;
-    let specVersion: string | undefined;
+    let specVersion: string = 'unknown';
     let specKeyword: string | undefined;
     let specFullVersion: string | undefined;
     let config: Config | undefined;
     const respectXSecurityAuthTypes: string[] = [];
     const collectSpecData: CollectFn = (document) => {
-      specVersion = detectSpec(document);
+      try {
+        specVersion = detectSpec(document);
+      } catch (err) {
+        specVersion = `unsupported`;
+      }
       if (!isPlainObject(document)) return;
       specKeyword = document?.openapi
         ? 'openapi'
