57f0b4872c35dec2181282eb3796cd6d113993f7
DmitryAnansky <dmytro@redocly.com>
2025-10-09 16:22:07 +0300

    chore: minor change

--- packages/cli/src/commands/respect/json-logs.ts
+++ packages/cli/src/commands/respect/json-logs.ts
@@ -89,7 +89,7 @@ function mapJsonStep(
       body: publicStep.response?.body,
       headers: step.response?.header || {},
       time: step.response?.time || 0,
-      size: step.response?.responseSize || undefined,
+      size: step.response?.responseSize,
     },
     checks: step.checks.map((check) => ({
       ...check,
