4cf854e938683388dcd66e737295539e60594e3c
DmitryAnansky <dmytro@redocly.com>
2025-10-15 17:34:25 +0300

    chore: cleanup test import

--- packages/core/src/config/__tests__/config-resolvers.test.ts
+++ packages/core/src/config/__tests__/config-resolvers.test.ts
@@ -1,16 +1,13 @@
 import util from 'node:util';
-import { colorize } from '../../logger.js';
 import { Asserts, asserts } from '../../rules/common/assertions/asserts.js';
 import { resolveConfig } from '../config-resolvers.js';
 import recommended from '../recommended.js';
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
-import { createRequire } from 'node:module';
 
 import type { RawUniversalConfig, RawGovernanceConfig } from '../types.js';
 import { Source } from '../../resolve.js';
 import { Config } from '../config.js';
-import { SpecVersion } from '../../oas-types.js';
 import { after } from 'node:test';
 
 vi.mock('node:module', () => ({
