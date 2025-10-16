45aaa49377acf055446d8f2cd34128e5c9d19269
DmitryAnansky <dmytro@redocly.com>
2025-10-16 18:36:14 +0300

    fix: applied proxy to respect requests

--- packages/cli/src/utils/proxy-agent.ts
+++ packages/cli/src/utils/proxy-agent.ts
@@ -1,6 +1,10 @@
 import { HttpsProxyAgent } from 'https-proxy-agent';
 
+export function getProxyUrl(): string | undefined {
+  return process.env.HTTPS_PROXY || process.env.HTTP_PROXY;
+}
+
 export function getProxyAgent() {
-  const proxy = process.env.HTTPS_PROXY || process.env.HTTP_PROXY;
+  const proxy = getProxyUrl();
   return proxy ? new HttpsProxyAgent(proxy) : undefined;
 }
