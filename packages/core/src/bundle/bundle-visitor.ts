1a8fea3f567a0955e7963f772b4bf40023576bfa
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-11-04 17:41:19 +0200

    chore: remove comment

--- packages/core/src/bundle/bundle-visitor.ts
+++ packages/core/src/bundle/bundle-visitor.ts
@@ -187,7 +187,6 @@ export function makeBundleVisitor(
   }
 
   function resolveBundledComponent(node: OasRef, resolved: ResolveResult<any>) {
-    // make ref from the root document to the bundled component
     const newRefId = makeRefId(rootDocument.source.absoluteRef, node.$ref);
     resolvedRefMap.set(newRefId, {
       document: rootDocument,
