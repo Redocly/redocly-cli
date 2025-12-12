b3d6fbbfaeb00559ac07104b1a5abbef52182188
DmitryAnansky <dmytro@redocly.com>
2025-12-12 12:40:51 +0200

    feat: update parsePointer in codeframes

--- packages/core/src/format/codeframes.ts
+++ packages/core/src/format/codeframes.ts
@@ -1,5 +1,5 @@
 import * as yamlAst from 'yaml-ast-parser';
-import { unescapePointer } from '../ref-utils.js';
+import { unescapePointerFromURI } from '../ref-utils.js';
 import { colorize, colorOptions } from '../logger.js';
 
 import type { LineColLocationObject, Loc, LocationObject } from '../walk.js';
@@ -16,7 +16,7 @@ const MAX_CODEFRAME_LINES = 3;
 
 // TODO: temporary
 function parsePointer(pointer: string) {
-  return pointer.substr(2).split('/').map(unescapePointer);
+  return pointer.substr(2).split('/').map(unescapePointerFromURI);
 }
 
 export function getCodeframe(location: LineColLocationObject, color: boolean) {
