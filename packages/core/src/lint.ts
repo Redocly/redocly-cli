d3ac6b3c364de85dafd0f2fb49be1f9b1799b440
Illia <illia.adamchuk@redocly.com>
2026-01-13 11:23:07 +0200

    fix: remove mutation of singleton

--- packages/core/src/lint.ts
+++ packages/core/src/lint.ts
@@ -33,9 +33,11 @@ import type {
 import type { CollectFn } from './utils/types.js';
 import type { JSONSchema } from 'json-schema-to-ts';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore FIXME: remove this once we remove `theme` from the schema
-delete rootRedoclyConfigSchema.properties.theme;
+const { theme: _, ...propertiesWithoutTheme } = rootRedoclyConfigSchema.properties;
+const redoclyConfigSchemaWithoutTheme = {
+  ...rootRedoclyConfigSchema,
+  properties: propertiesWithoutTheme,
+};
 
 export async function lint(opts: {
   ref: string;
@@ -150,7 +152,7 @@ export async function lintConfig(opts: {
   };
 
   const types = normalizeTypes(
-    opts.externalConfigTypes || createConfigTypes(rootRedoclyConfigSchema, config)
+    opts.externalConfigTypes || createConfigTypes(redoclyConfigSchemaWithoutTheme, config)
   );
 
   const rules: (RuleInstanceConfig & {
