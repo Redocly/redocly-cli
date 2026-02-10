55f7b0188b38d90ba28b0cc3cbd2d7dd188d9b59
Tiberiu Sabău <T.Sabau@student.tudelft.nl>
2026-02-10 16:33:39 +0100

    Add AsyncAPI support in the stats command

---
+++ .changeset/modern-coats-sink.md
@@ -0,0 +1,5 @@
+---
+"@redocly/openapi-core": major
+---
+
+Add AsyncAPI support in the stats command
--- tests/e2e/commands.test.ts
+++ tests/e2e/commands.test.ts
@@ -966,5 +966,26 @@ describe('E2E', () => {
       const result = getCommandOutput(args, { testPath });
       await expect(cleanupOutput(result)).toMatchFileSnapshot(join(testPath, 'snapshot.txt'));
     });
+
+    test('stats should support AsyncAPI 2.x (stylish format)', async () => {
+      const testPath = join(folderPath, 'stats-async2-stylish');
+      const args = getParams(indexEntryPoint, ['stats', 'async.yaml']);
+      const result = getCommandOutput(args, { testPath });
+      await expect(cleanupOutput(result)).toMatchFileSnapshot(join(testPath, 'snapshot.txt'));
+    });
+
+    test('stats should support AsyncAPI 2.x (JSON format)', async () => {
+      const testPath = join(folderPath, 'stats-async2-json');
+      const args = getParams(indexEntryPoint, ['stats', 'async.yaml', '--format=json']);
+      const result = getCommandOutput(args, { testPath });
+      await expect(cleanupOutput(result)).toMatchFileSnapshot(join(testPath, 'snapshot.txt'));
+    });
+
+    test('stats should support AsyncAPI 3.x (stylish format)', async () => {
+      const testPath = join(folderPath, 'stats-async3-stylish');
+      const args = getParams(indexEntryPoint, ['stats', 'asyncapi3.yaml']);
+      const result = getCommandOutput(args, { testPath });
+      await expect(cleanupOutput(result)).toMatchFileSnapshot(join(testPath, 'snapshot.txt'));
+    });
   });
 });
