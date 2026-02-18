323b9df87aa87544fc083abf6baca8ed225b2efe
Albina Blazhko <albina.blazhko@redocly.com>
2026-02-18 17:24:23 +0200

    chore: fix prettier error

--- packages/core/src/utils/scorecards.ts
+++ packages/core/src/utils/scorecards.ts
@@ -1,7 +1,7 @@
 // Fix issue on yarn and windows with dynamic import of @redocly/config
-import { createRequire } from 'module'
-const require = createRequire(import.meta.url)
-const { NODE_TYPE_NAMES } = require('@redocly/config')
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const { NODE_TYPE_NAMES } = require('@redocly/config');
 
 import { dequal } from './dequal.js';
 import { isPlainObject } from './is-plain-object.js';
