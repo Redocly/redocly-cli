f8fd1855a1e91d56f306f83771a30d627cfe00d6
Harshit Singh <harshitsingh467@gmail.com>
2026-04-06 17:23:21 +0530

    feat: add function to remove discriminator keywords from schema

---
+++ packages/respect-core/src/utils/remove-discriminators-from-schema.ts
@@ -0,0 +1,29 @@
+/**
+ * removes `discriminator` keywords from a fully-dereferenced schema.
+ */
+export function removeDiscriminatorsFromSchema(schema: any): any {
+  if (!schema || typeof schema !== 'object') return schema;
+
+  const schemaCopy = JSON.parse(JSON.stringify(schema));
+  stripDiscriminators(schemaCopy);
+  return schemaCopy;
+}
+
+function stripDiscriminators(node: any): void {
+  if (!node || typeof node !== 'object') return;
+
+  if (Array.isArray(node)) {
+    for (const item of node) {
+      stripDiscriminators(item);
+    }
+    return;
+  }
+
+  if ('discriminator' in node) {
+    delete node.discriminator;
+  }
+
+  for (const key of Object.keys(node)) {
+    stripDiscriminators(node[key]);
+  }
+}
