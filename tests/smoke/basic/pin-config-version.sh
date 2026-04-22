1dcd7fbe5442a063ea08833a3a5544bfb16668e5
Albina Blazhko <albina.blazhko@redocly.com>
2026-04-22 16:33:48 +0300

    chore: fix config version script

--- tests/smoke/basic/pin-config-version.sh
+++ tests/smoke/basic/pin-config-version.sh
@@ -9,7 +9,9 @@ node -e "
   const fs = require('fs');
   const path = './tests/smoke/basic/package.json';
   const pkg = JSON.parse(fs.readFileSync(path, 'utf8'));
-  pkg.resolutions['@redocly/config'] = '$TARBALL_REF';
-  pkg.overrides['@redocly/config'] = '$TARBALL_REF';
+  (pkg.resolutions ??= {})['@redocly/config'] = '$TARBALL_REF';
+  (pkg.overrides ??= {})['@redocly/config'] = '$TARBALL_REF';
+  ((pkg.pnpm ??= {}).overrides ??= {})['@redocly/config'] = '$REDOCLY_CONFIG_VERSION';
+
   fs.writeFileSync(path, JSON.stringify(pkg, null, 2) + '\n');
 "
