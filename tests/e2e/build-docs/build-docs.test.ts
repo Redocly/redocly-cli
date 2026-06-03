7b5e572950ffc00982527ba04b81abe836478cef
Andrew Tatomyr <andrew.tatomyr@redocly.com>
2026-06-03 15:33:21 +0300

    chore(cli): update redoc to v2.5.3 (#2842)

--- tests/e2e/build-docs/build-docs.test.ts
+++ tests/e2e/build-docs/build-docs.test.ts
@@ -38,7 +38,7 @@ describe('build-docs', () => {
     `);
 
     expect(existsSync(join(testPath, 'nested/redoc-static.html'))).toEqual(true);
-    expect(statSync(join(testPath, 'nested/redoc-static.html')).size).toEqual(36414);
+    expect(statSync(join(testPath, 'nested/redoc-static.html')).size).toEqual(36375);
     const output = readFileSync(join(testPath, 'nested/redoc-static.html'), 'utf8');
     await expect(output).toMatchFileSnapshot(join(testPath, 'snapshot.txt'));
   });
