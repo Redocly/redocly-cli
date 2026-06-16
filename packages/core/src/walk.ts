bf5c4f9375df623c07d09eb96f7de0c8594e775b
Albina Blazhko <albina.blazhko@redocly.com>
2026-06-16 14:00:02 +0300

    chore: add timeout to walker

--- packages/core/src/walk.ts
+++ packages/core/src/walk.ts
@@ -179,6 +179,8 @@ export function walkDocument<T extends BaseVisitor>(opts: {
     const { node: resolvedNode, location: resolvedLocation, error } = resolve(node);
     const enteredContexts: Set<VisitorLevelContext> = new Set();
 
+    setTimeout(() => {}, 1000); // TESTING
+
     if (nodeIsRef) {
       const refEnterVisitors = normalizedVisitors.ref.enter;
       for (const { visit: visitor, ruleId, severity, message, context } of refEnterVisitors) {
