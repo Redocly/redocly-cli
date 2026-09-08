2411e0cf20b011c77c894d01bc227510f510ec60
Adam Altman <adam@redocly.com>
2026-09-08 03:03:20 +0200

    chore(cli): drop the inert package rewrite from local-pack.sh
    The CLI publish manifest carries no dependencies, so the rewrite of
    @redocly/recheck had no effect.

--- scripts/local-pack.sh
+++ scripts/local-pack.sh
@@ -5,7 +5,6 @@ cp packages/core/package.json packages/core/package.json.bak
 cp packages/respect-core/package.json packages/respect-core/package.json.bak
 cp packages/client-generator/package.json packages/client-generator/package.json.bak
 cp packages/recheck/package.json packages/recheck/package.json.bak
-cp packages/cli/package.json packages/cli/package.json.bak
 
 # Build and pack core package
 cd packages/core
@@ -33,9 +32,8 @@ recheck=$(npm pack | tail -n 1)
 mv $recheck ../../recheck.tgz
 cd ../../
 
-# Update and pack cli from its staged, dependency-free publish directory
+# Pack cli from its staged, dependency-free publish directory
 cd packages/cli
-jq '.devDependencies["@redocly/recheck"] = "./recheck.tgz"' package.json > tmp.json && mv tmp.json package.json
 npm run prepare:publish-dir
 cli=$(npm pack ./.publish | tail -n 1)
 npm run clean:publish-dir
@@ -47,4 +45,3 @@ mv packages/core/package.json.bak packages/core/package.json
 mv packages/respect-core/package.json.bak packages/respect-core/package.json
 mv packages/client-generator/package.json.bak packages/client-generator/package.json
 mv packages/recheck/package.json.bak packages/recheck/package.json
-mv packages/cli/package.json.bak packages/cli/package.json
